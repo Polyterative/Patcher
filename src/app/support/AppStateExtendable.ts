deb48bd9024c87ade1dd07a95dcfe79a21518a56
vlady <vlady.internet@outlook.it>
2019-04-11 10:59:56 +0200

    progress

--- src/app/support/AppStateExtendable.ts
+++ src/app/support/AppStateExtendable.ts
@@ -1,19 +1,21 @@
 import {
   Emittable,
   Emitter
-} from '@ngxs-labs/emitter';
+}                     from '@ngxs-labs/emitter';
+import { Select }     from '@ngxs/store';
+import { Observable } from 'rxjs';
 import {
   AppState,
   PageStatusCases
-} from 'src/app/store/app.state';
+}                     from 'src/app/store/app.state';
 
 export class AppStateExtendable {
   
   // @Select(TitleState)
   // title$: Observable<TitleStateModel>;
   //
-  // @Select(PageLoadingState)
-  // pageLoading$: Observable<PageLoadingStateModel>;
+  @Select(AppState.pageTitle)
+  pageTitle$: Observable<string>;
   
   @Emitter(AppState.setPageStatus)
   setTitle$: Emittable<PageStatusCases>;
