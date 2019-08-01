3f9b43902cb6142354124676602b00cf6c868f52
vlady <vlady.internet@outlook.it>
2019-08-01 16:13:25 +0200

    progress

---
+++ src/app/blog/blog-post/blog-post.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BlogPostComponent } from './blog-post.component';
+
+@NgModule({
+  declarations: [BlogPostComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class BlogPostModule { }
