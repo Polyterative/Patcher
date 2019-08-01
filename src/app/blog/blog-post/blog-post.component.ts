e76b13a4dfca3f1e569ebb28ca5363f3b228fc22
vlady <vlady.internet@outlook.it>
2019-08-01 16:41:39 +0200

    progress

--- src/app/blog/blog-post/blog-post.component.ts
+++ src/app/blog/blog-post/blog-post.component.ts
@@ -1,19 +1,26 @@
 import {
   Component,
   OnInit
-} from '@angular/core';
+}                         from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
-  selector: 'app-blog-post',
+  selector:    'app-blog-post',
   templateUrl: './blog-post.component.html',
-  styleUrls: ['./blog-post.component.scss']
+  styleUrls:   ['./blog-post.component.scss']
 })
 export class BlogPostComponent implements OnInit {
   
-  constructor() {
+  post: string;
+  
+  constructor(private route: ActivatedRoute) {
+  
   }
   
   ngOnInit() {
+    this.route.params.subscribe(params => {
+      this.post = `./assets/blog/post/${ params.id }.md`;
+    });
   }
   
 }
--- src/app/blog/blog.module.ts
+++ src/app/blog/blog.module.ts
@@ -1,10 +1,17 @@
-import { CommonModule } from '@angular/common';
-import { NgModule }     from '@angular/core';
+import { CommonModule }   from '@angular/common';
+import { NgModule }       from '@angular/core';
+import { RouterModule }   from '@angular/router';
+import { MarkdownModule } from 'ngx-markdown';
 
 @NgModule({
   declarations: [],
   imports:      [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild([
+      {path: '', pathMatch: 'full', loadChildren: './blog/blog-view.module#BlogViewModule'},
+      {path: 'post', loadChildren: './post/blog-post-view.module#BlogPostModule'}
+    ]),
+    MarkdownModule.forRoot()
   ]
 })
 export class BlogModule {}
