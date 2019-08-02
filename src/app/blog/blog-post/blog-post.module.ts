ca04bec62436161850e2c66072f45ed5b25b89b9
vlady <vlady.internet@outlook.it>
2019-08-02 16:33:05 +0200

    progress, cuter

--- src/app/blog/blog-post/blog-post.module.ts
+++ src/app/blog/blog-post/blog-post.module.ts
@@ -1,19 +1,25 @@
-import { CommonModule }      from '@angular/common';
-import { NgModule }          from '@angular/core';
-import { RouterModule }      from '@angular/router';
-import { MarkdownModule }    from 'ngx-markdown';
-import { BlogPostComponent } from './blog-post.component';
+import { CommonModule }           from '@angular/common';
+import { NgModule }               from '@angular/core';
+import { FlexLayoutModule }       from '@angular/flex-layout';
+import { MatCardModule }          from '@angular/material';
+import { RouterModule }           from '@angular/router';
+import { MarkdownModule }         from 'ngx-markdown';
+import { OrangeStructuresModule } from '../../Utils/LocalLibraries/OrangeStructures/orange-structures.module';
+import { BlogPostComponent }      from './blog-post.component';
 
 @NgModule({
   declarations: [BlogPostComponent],
-  imports:      [
+  imports: [
     CommonModule,
     RouterModule.forChild([
       {path: '', component: BlogPostComponent},
       {path: ':id', component: BlogPostComponent, pathMatch: 'full'}
     ]),
     // ComponentsModule,
-    MarkdownModule.forChild()
+    MarkdownModule.forChild(),
+    OrangeStructuresModule,
+    MatCardModule,
+    FlexLayoutModule
   ]
 })
 export class BlogPostModule {}
