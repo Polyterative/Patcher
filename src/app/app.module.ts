3c55818b170c0b047dd9be1570400f742f90d8c3
vlady <vlady.internet@outlook.it>
2019-08-02 10:34:58 +0200

    progress

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,17 +14,17 @@ import {
 }                                  from '@angular/material/core';
 import { BrowserModule }           from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { environment }             from '../environments/environment';
 
-import { MaterialModulePack }        from 'src/app/material/material-module-pack.module';
-import { LandingPageComponent }      from 'src/app/Pages/landing-page/landing-page.component';
-import { MatFormEntityModule }       from 'src/app/Utils/LocalLibraries/mat-form-entity/mat-form-entity.module';
-import { OrangeStructuresModule }    from 'src/app/Utils/LocalLibraries/OrangeStructures/orange-structures.module';
-import { VioletUtilsModule }         from 'src/app/Utils/LocalLibraries/VioletUtilities/violet-utils.module';
-import { PolyUtilsComponentsModule } from 'src/app/Utils/poly-utils-components.module';
-import { environment }               from 'src/environments/environment';
 import { AppRoutingModule }          from './app-routing.module';
 import { AppComponent }              from './app.component';
 import { BlogModule }                from './blog/blog.module';
+import { MaterialModulePack }        from './material/material-module-pack.module';
+import { LandingPageComponent }      from './Pages/landing-page/landing-page.component';
+import { MatFormEntityModule }       from './Utils/LocalLibraries/mat-form-entity/mat-form-entity.module';
+import { OrangeStructuresModule }    from './Utils/LocalLibraries/OrangeStructures/orange-structures.module';
+import { VioletUtilsModule }         from './Utils/LocalLibraries/VioletUtilities/violet-utils.module';
+import { PolyUtilsComponentsModule } from './Utils/poly-utils-components.module';
 
 const italianLocale: { useValue: string; provide: InjectionToken<string>; } = {
   provide:  LOCALE_ID,
