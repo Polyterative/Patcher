b8783bf7c68bc01d91f7a9208da2a686682fc464
vlady <vlady.internet@outlook.it>
2019-08-05 17:36:47 +0200

    progress

--- src/app/Utils/Components/card-wrapper/card-wrapper.module.ts
+++ src/app/Utils/Components/card-wrapper/card-wrapper.module.ts
@@ -1,12 +1,22 @@
-import { CommonModule }         from '@angular/common';
-import { NgModule }             from '@angular/core';
-import { CardWrapperComponent } from 'src/app/Utils/Components/card-wrapper/card-wrapper.component';
+import { CommonModule }           from '@angular/common';
+import { NgModule }               from '@angular/core';
+import { FlexModule }             from '@angular/flex-layout';
+import {
+  MatCardModule,
+  MatExpansionModule
+}                                 from '@angular/material';
+import { CardWrapperComponent }   from 'src/app/Utils/Components/card-wrapper/card-wrapper.component';
+import { OrangeStructuresModule } from '../../LocalLibraries/OrangeStructures/orange-structures.module';
 
 @NgModule({
   declarations: [CardWrapperComponent],
   exports:      [CardWrapperComponent],
-  imports:      [
-    CommonModule
+  imports: [
+    CommonModule,
+    MatExpansionModule,
+    MatCardModule,
+    FlexModule,
+    OrangeStructuresModule
   ]
 })
 export class CardWrapperModule {
