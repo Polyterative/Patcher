430a201cb6a375c62fa8c42813a94d0dc00163aa
vlady <vlady.internet@outlook.it>
2019-08-05 17:21:27 +0200

    progress

---
+++ src/app/Utils/LocalLibraries/OrangeStructures/flex/flex-row-wrap/flex-row-wrap.component.ts
@@ -0,0 +1,14 @@
+import {
+  ChangeDetectionStrategy,
+  Component
+}                   from '@angular/core';
+import { FlexBase } from '../flex-base';
+
+@Component({
+  selector:        'lib-flex-row-wrap',
+  templateUrl:     './flex-row-wrap.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class FlexRowWrapComponent extends FlexBase {
+  
+}
