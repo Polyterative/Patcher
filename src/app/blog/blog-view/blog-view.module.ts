75237a7c170cb7c62976055801b90b72adef1178
vlady <vlady.internet@outlook.it>
2019-08-05 17:18:31 +0200

    progress

--- src/app/blog/blog-view/blog-view.module.ts
+++ src/app/blog/blog-view/blog-view.module.ts
@@ -1,7 +1,10 @@
 import { CommonModule }           from '@angular/common';
 import { NgModule }               from '@angular/core';
 import { FlexLayoutModule }       from '@angular/flex-layout';
-import { MatCardModule }          from '@angular/material';
+import {
+  MatButtonModule,
+  MatCardModule
+} from '@angular/material';
 import { RouterModule }           from '@angular/router';
 import { MarkdownModule }         from 'ngx-markdown';
 import { OrangeStructuresModule } from '../../Utils/LocalLibraries/OrangeStructures/orange-structures.module';
@@ -15,7 +18,8 @@ import { BlogViewComponent }      from './blog-view.component';
     MarkdownModule.forChild(),
     OrangeStructuresModule,
     FlexLayoutModule,
-    MatCardModule
+    MatCardModule,
+    MatButtonModule
   ]
 })
 export class BlogViewModule {}
