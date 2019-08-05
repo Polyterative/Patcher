eb504fcd180ede3fa2b8f788b0b3b5ed419daeb5
vlady <vlady.internet@outlook.it>
2019-08-05 10:37:04 +0200

    progress

--- src/app/blog/blog-view/blog-view.component.ts
+++ src/app/blog/blog-view/blog-view.component.ts
@@ -1,19 +1,51 @@
-import {
-  Component,
-  OnInit
-} from '@angular/core';
+import { Component }         from '@angular/core';
+import { AngularFirestore }  from '@angular/fire/firestore';
+import { FormBuilder }       from '@angular/forms';
+import { MatSnackBar }       from '@angular/material';
+import { DateTime }          from 'luxon';
+import { Observable }        from 'rxjs';
+import { AngularEntityBase } from '../../Utils/LocalLibraries/OrangeStructures/base/angularEntityBase';
+import { ConstantsService }  from '../../Utils/LocalLibraries/VioletUtilities/constants.service';
+import { DimensionsService } from '../../Utils/LocalLibraries/VioletUtilities/dimensions.service';
+
+enum Category {
+  'Music',
+  'Code',
+  'Productivity'
+}
+
+interface BlogPostModel {
+  title: string;
+  subtitle: string;
+  content: string;
+  created: DateTime;
+  updated: DateTime;
+  category: Category;
+}
 
 @Component({
   selector:    'app-blog-view',
   templateUrl: './blog-view.component.html',
   styleUrls:   ['./blog-view.component.scss']
 })
-export class BlogViewComponent implements OnInit {
+export class BlogViewComponent extends AngularEntityBase {
   
-  constructor() {
-  }
+  messages$: Observable<BlogPostModel[]>;
+  private blogPostPath = 'blogPosts';
   
-  ngOnInit() {
+  constructor(
+    db: AngularFirestore,
+    public constants: ConstantsService,
+    public dimens: DimensionsService,
+    private formBuilder: FormBuilder,
+    public snackbar: MatSnackBar
+  ) {
+    super(constants, dimens);
+    
+    // @ts-ignore
+    this.messages$ = db.collection(this.blogPostPath, ref => ref.limit(10).orderBy('when', 'desc'))
+      .valueChanges()
+    ;
   }
   
 }
--- src/app/blog/blog-view/blog-view.module.ts
+++ src/app/blog/blog-view/blog-view.module.ts
@@ -1,18 +1,24 @@
-import { CommonModule }      from '@angular/common';
-import { NgModule }          from '@angular/core';
-import { RouterModule }      from '@angular/router';
-import { MarkdownModule }    from 'ngx-markdown';
-import { BlogViewComponent } from './blog-view.component';
+import { CommonModule }           from '@angular/common';
+import { NgModule }               from '@angular/core';
+import { FlexLayoutModule }       from '@angular/flex-layout';
+import { MatCardModule }          from '@angular/material';
+import { RouterModule }           from '@angular/router';
+import { MarkdownModule }         from 'ngx-markdown';
+import { OrangeStructuresModule } from '../../Utils/LocalLibraries/OrangeStructures/orange-structures.module';
+import { BlogViewComponent }      from './blog-view.component';
 
 @NgModule({
   declarations: [BlogViewComponent],
-  imports:      [
+  imports: [
     CommonModule,
     RouterModule.forChild([
       {path: '', component: BlogViewComponent}
     ]),
     // ComponentsModule,
-    MarkdownModule.forChild()
+    MarkdownModule.forChild(),
+    OrangeStructuresModule,
+    FlexLayoutModule,
+    MatCardModule
   ]
 })
 export class BlogViewModule {}
