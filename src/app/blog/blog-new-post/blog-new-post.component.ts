cde16f2a878ee2e29ccfc91a838d3468285a0087
vlady <vlady.internet@outlook.it>
2019-08-06 11:56:12 +0200

    progress

--- src/app/blog/blog-new-post/blog-new-post.component.ts
+++ src/app/blog/blog-new-post/blog-new-post.component.ts
@@ -1,20 +1,28 @@
 import {
   Component,
   OnInit,
-  ChangeDetectionStrategy
-}                            from '@angular/core';
-import { AngularFirestore }  from '@angular/fire/firestore';
+  ChangeDetectionStrategy,
+  EventEmitter
+}                             from '@angular/core';
+import { AngularFirestore }   from '@angular/fire/firestore';
 import {
   FormBuilder,
   FormControl,
   Validators
-}                            from '@angular/forms';
-import { MatSnackBar }       from '@angular/material';
-import { ActivatedRoute }    from '@angular/router';
-import { FormTypes }         from '../../Utils/LocalLibraries/mat-form-entity/form-element-models';
-import { AngularEntityBase } from '../../Utils/LocalLibraries/OrangeStructures/base/angularEntityBase';
-import { ConstantsService }  from '../../Utils/LocalLibraries/VioletUtilities/constants.service';
-import { DimensionsService } from '../../Utils/LocalLibraries/VioletUtilities/dimensions.service';
+}                             from '@angular/forms';
+import { MatSnackBar }        from '@angular/material';
+import { ActivatedRoute }     from '@angular/router';
+import { DateTime }           from 'luxon';
+import {
+  map,
+  tap
+}                             from 'rxjs/operators';
+import { FormTypes }          from '../../Utils/LocalLibraries/mat-form-entity/form-element-models';
+import { AngularEntityBase }  from '../../Utils/LocalLibraries/OrangeStructures/base/angularEntityBase';
+import { ConstantsService }   from '../../Utils/LocalLibraries/VioletUtilities/constants.service';
+import { DimensionsService }  from '../../Utils/LocalLibraries/VioletUtilities/dimensions.service';
+import { CommunicationUtils } from '../../Utils/LocalLibraries/VioletUtilities/general-utils';
+import { BlogPostModel }      from '../blog-view/blog-view.component';
 
 @Component({
   selector:        'app-blog-new-post',
@@ -33,6 +41,9 @@ export class BlogNewPostComponent extends AngularEntityBase {
   
   formTypes = FormTypes;
   
+  confirm = new EventEmitter<void>();
+  
+  private blogPostPath = 'blogPosts';
   
   constructor(private route: ActivatedRoute,
               private db: AngularFirestore,
@@ -41,6 +52,36 @@ export class BlogNewPostComponent extends AngularEntityBase {
               public snackbar: MatSnackBar) {
     super();
     
+    this.confirm
+      .pipe(
+        map(_ => {
+          
+          const dateTime = DateTime.local();
+          const message: BlogPostModel = {
+            content:  this.controls.content.value,
+            title:    this.controls.title.value,
+            subtitle: this.controls.subtitle.value,
+            category: this.controls.category.value,
+            created:  dateTime.toISO(),
+            updated:  dateTime.toISO(),
+            id:       3
+          };
+          
+          
+          return message;
+        })
+        // tap(_ => this.controls.content.reset())
+      )
+      .subscribe(x => {
+        
+        
+        db.collection(this.blogPostPath).add(x);
+        
+        CommunicationUtils.showSnackbar(this.snackbar, 'Aggiunto');
+        
+      });
+    
   }
+  
   
 }
