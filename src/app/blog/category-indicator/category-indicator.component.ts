8f4b7f98b8fa52004a1b967a04f9a6ad7b523a9a
vlady <vlady.internet@outlook.it>
2019-08-08 09:50:31 +0200

    progress

---
+++ src/app/blog/category-indicator/category-indicator.component.ts
@@ -0,0 +1,14 @@
+import {
+  ChangeDetectionStrategy,
+  Component
+} from '@angular/core';
+
+@Component({
+  selector:        'app-category-indicator',
+  templateUrl:     './category-indicator.component.html',
+  styleUrls:       ['./category-indicator.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class CategoryIndicatorComponent {
+
+}
