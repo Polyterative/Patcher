996beab2b4edf2d529c3b4672a3460d2bd3e74a6
vlady <vlady.internet@outlook.it>
2019-08-09 11:09:17 +0200

    progress

--- src/app/blog/about-me/about-me.module.ts
+++ src/app/blog/about-me/about-me.module.ts
@@ -1,12 +1,14 @@
-import { CommonModule }     from '@angular/common';
-import { NgModule }         from '@angular/core';
-import { AboutMeComponent } from './about-me.component';
+import { CommonModule }      from '@angular/common';
+import { NgModule }          from '@angular/core';
+import { WordSwapperModule } from '../word-swapper/word-swapper.module';
+import { AboutMeComponent }  from './about-me.component';
 
 
 @NgModule({
   declarations: [AboutMeComponent],
   imports:      [
-    CommonModule
+    CommonModule,
+    WordSwapperModule
   ],
   exports:      [AboutMeComponent]
 })
