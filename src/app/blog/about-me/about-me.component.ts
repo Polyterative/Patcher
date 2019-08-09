1d5380ce939ba2a524dcfc41bdd389675c191435
vlady <vlady.internet@outlook.it>
2019-08-09 11:03:17 +0200

    progress

---
+++ src/app/blog/about-me/about-me.component.ts
@@ -0,0 +1,21 @@
+import {
+  ChangeDetectionStrategy,
+  Component,
+  OnInit
+} from '@angular/core';
+
+@Component({
+  selector:        'app-about-me',
+  templateUrl:     './about-me.component.html',
+  styleUrls:       ['./about-me.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class AboutMeComponent implements OnInit {
+  
+  constructor() {
+  }
+  
+  ngOnInit() {
+  }
+  
+}
