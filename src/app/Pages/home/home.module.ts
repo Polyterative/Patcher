7c5b19c59c19ea5e0bbb2c2cae909f5315bafca6
vlady <vlady.internet@outlook.it>
2019-08-12 16:00:20 +0200

    progress

--- src/app/Pages/home/home.module.ts
+++ src/app/Pages/home/home.module.ts
@@ -1,7 +1,10 @@
 import { CommonModule }            from '@angular/common';
 import { NgModule }                from '@angular/core';
 import { FlexLayoutModule }        from '@angular/flex-layout';
-import { MatCardModule }           from '@angular/material';
+import {
+  MatButtonModule,
+  MatCardModule
+}                                  from '@angular/material';
 import { BlogPostStructureModule } from '../../blog/blog-post-structure/blog-post-structure.module';
 import { PageRetrieverModule }     from '../../blog/page-retriever/page-retriever.module';
 import { WordSwapperModule }       from '../../blog/word-swapper/word-swapper.module';
@@ -17,7 +20,8 @@ import { HomeComponent }           from './home.component';
     MatCardModule,
     BlogPostStructureModule,
     PageRetrieverModule,
-    WordSwapperModule
+    WordSwapperModule,
+    MatButtonModule
   ],
   exports:      [HomeComponent]
 })
