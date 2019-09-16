ce6bb9bb6f3bbaa1d891e81edd5db8638ae448a0
vlady <vlady.internet@outlook.it>
2019-09-16 16:44:58 +0200

    style

--- src/app/blog/blog-new-post/blog-new-post.component.ts
+++ src/app/blog/blog-new-post/blog-new-post.component.ts
@@ -58,11 +58,11 @@ export class BlogNewPostComponent extends AngularEntityBase {
             Validators.minLength(5)
         ]))
     };
-
+    
     edit = {
         postOptions: new BehaviorSubject<Array<Selectable>>([])
     };
-
+    
     formTypes = FormTypes;
     confirm = new EventEmitter<void>();
     kindOptions: Array<Selectable> = [
@@ -70,73 +70,73 @@ export class BlogNewPostComponent extends AngularEntityBase {
         {id: '1', name: 'Post'}
     ];
     isEditing = false;
-
+    
     // post$: BehaviorSubject<BlogEntryModel | undefined> = new BehaviorSubject<BlogEntryModel>(undefined);
-
+    
     constructor(private route: ActivatedRoute,
                 private dataservice: FirebaseService,
                 public constants: ConstantsService,
                 public dimens: DimensionsService,
                 public snackbar: MatSnackBar) {
         super();
-
+        
         this.route.params
-          .pipe(
-            map(x => x.slug),
-            filter(x => x),
-            take(1),
-            switchMap(x => dataservice.getBlogPost(x))
-          )
-          .subscribe((x: BlogEntryModel) => {
-              this.isEditing = true;
-              this.controls.slug.patchValue(x.slug);
-              this.controls.title.patchValue(x.title);
-              this.controls.subtitle.patchValue(x.subtitle);
-              this.controls.kind.patchValue('1');
-              this.controls.content.patchValue(x.content);
-              this.controls.created.patchValue(x.created);
-              this.controls.updated.patchValue(x.updated);
-          });
-
+            .pipe(
+                map(x => x.slug),
+                filter(x => x),
+                take(1),
+                switchMap(x => dataservice.getBlogPost(x))
+            )
+            .subscribe((x: BlogEntryModel) => {
+                this.isEditing = true;
+                this.controls.slug.patchValue(x.slug);
+                this.controls.title.patchValue(x.title);
+                this.controls.subtitle.patchValue(x.subtitle);
+                this.controls.kind.patchValue('1');
+                this.controls.content.patchValue(x.content);
+                this.controls.created.patchValue(x.created);
+                this.controls.updated.patchValue(x.updated);
+            });
+        
         this.confirm
-          .pipe(
-            takeUntil(this.destroyEvent$),
-            map(() => {
-                const dateTime = DateTime.local().toISO();
-
-                this.controls.updated.patchValue(dateTime);
-
-                if (!this.isEditing) {
-                    this.controls.created.patchValue(dateTime);
-                }
-
-                const message: BlogEntryModel = {
-                    public:   true,
-                    content:  this.controls.content.value,
-                    title:    this.controls.title.value,
-                    subtitle: this.controls.subtitle.value,
-                    category: this.controls.category.value,
-                    slug:     this.controls.slug.value,
-                    created:  this.controls.created.value,
-                    updated:  this.controls.updated.value
-                };
-
-                return message;
-            })
-            // tap(_ => this.controls.content.reset())
-          )
-          .subscribe(x => {
-              // tslint:disable-next-line:triple-equals
-              const path = this.controls.kind.value == '1' ? this.dataservice.blogPostPath : this.dataservice.pagesPath;
-
-              dataservice.add(path, x);
-              CommunicationUtils.showSnackbar(this.snackbar, 'Aggiunto');
-          });
-    
+            .pipe(
+                takeUntil(this.destroyEvent$),
+                map(() => {
+                    const dateTime = DateTime.local().toISO();
+            
+                    this.controls.updated.patchValue(dateTime);
+            
+                    if (!this.isEditing) {
+                        this.controls.created.patchValue(dateTime);
+                    }
+            
+                    const message: BlogEntryModel = {
+                        public:   true,
+                        content:  this.controls.content.value,
+                        title:    this.controls.title.value,
+                        subtitle: this.controls.subtitle.value,
+                        category: this.controls.category.value,
+                        slug:     this.controls.slug.value,
+                        created:  this.controls.created.value,
+                        updated:  this.controls.updated.value
+                    };
+            
+                    return message;
+                })
+                // tap(_ => this.controls.content.reset())
+            )
+            .subscribe(x => {
+                // tslint:disable-next-line:triple-equals
+                const path = this.controls.kind.value == '1' ? this.dataservice.blogPostPath : this.dataservice.pagesPath;
+        
+                dataservice.add(path, x);
+                CommunicationUtils.showSnackbar(this.snackbar, 'Aggiunto');
+            });
+        
         dataservice.getBlogPosts()
             .pipe(map(x => x.map((y: BlogEntryModel) => ({id: y.slug, name: y.title}))))
             .subscribe(this.edit.postOptions);
-
+        
     }
-
+    
 }
