cae4e67367954d3cfa9c9c6bc69acf500833846b
vlady <vlady.internet@outlook.it>
2019-11-29 11:23:57 +0100

    half broken login

--- src/app/Services/firebase.service.ts
+++ src/app/Services/firebase.service.ts
@@ -1,10 +1,15 @@
-import { Injectable }       from '@angular/core';
+import {
+    EventEmitter,
+    Injectable
+}                           from '@angular/core';
 import { AngularFireAuth }  from '@angular/fire/auth';
 import { AngularFirestore } from '@angular/fire/firestore';
 import * as firebase        from 'firebase';
+import { User }             from 'firebase';
 import {
+    concat,
     OperatorFunction,
-    Subject
+    ReplaySubject
 }                           from 'rxjs';
 import { fromPromise }      from 'rxjs/internal-compatibility';
 import {
@@ -12,10 +17,10 @@ import {
     map,
     mergeMap,
     switchMap,
-    take,
-    tap
+    take
 }                           from 'rxjs/operators';
 import { BlogEntryModel }   from '../blog/blog-models';
+import AuthPersistence = firebase.auth.Auth.Persistence;
 import GoogleAuthProvider = firebase.auth.GoogleAuthProvider;
 import UserCredential = firebase.auth.UserCredential;
 
@@ -26,32 +31,54 @@ export class FirebaseService {
     pagesPath = 'pages';
     blogPostPath = 'blogPosts';
     instaPath = 'insta-links';
-    user$: Subject<UserCredential> = new Subject();
+    user$: ReplaySubject<UserCredential | User> = new ReplaySubject();
+    
+    login$ = new EventEmitter<void>();
+    logout$ = new EventEmitter<void>();
     
     private mapToData: OperatorFunction<firebase.firestore.QueryDocumentSnapshot, firebase.firestore.DocumentData> = map(x => x.data());
     
     constructor(
-        private store: AngularFirestore,
-        private auth: AngularFireAuth
+        private fireStore: AngularFirestore,
+        private fireAuth: AngularFireAuth
     ) {
-    }
-    
-    login() {
-        const auth$ = fromPromise(this.auth.auth.signInWithPopup(new GoogleAuthProvider()));
-        auth$.pipe(filter(x => !(!x)), tap(x => console.log(x))).subscribe(this.user$);
-    }
-    
-    logout() {
-        return fromPromise(this.auth.auth.signOut());
+        
+        // external outlet
+        this.login$.subscribe(x => {
+            this.firebaseLogin();
+        });
+        
+        // external outlet
+        this.logout$
+            .subscribe(x => {
+                this.user$.next(undefined);
+                
+                this.firebaseLogout();
+            });
+        
+        // login
+        // fireAuth.user
+        //     .pipe(
+        //         filter(x => x && x.emailVerified && !!x.displayName)
+        //     )
+        //     .subscribe(x => {
+        //         this.user$.next(x);
+        //     });
+        //
+        // this.user$.subscribe(x => {
+        //     console.warn(x);
+        // });
     }
     
     getPage(slug) {
-        return this.getSingleWithSlug(this.pagesPath, slug).pipe(this.mapToData);
+        return this.getSingleWithSlug(this.pagesPath, slug)
+            .pipe(this.mapToData);
         
     }
     
     getBlogPost(slug) {
-        return this.getSingleWithSlug(this.blogPostPath, slug).pipe(this.mapToData);
+        return this.getSingleWithSlug(this.blogPostPath, slug)
+            .pipe(this.mapToData);
     }
     
     getBlogPosts(limit?: number) {
@@ -59,13 +86,15 @@ export class FirebaseService {
     }
     
     add(path: string, data) {
-        return fromPromise(this.store.collection(path).add(data));
+        return fromPromise(this.fireStore.collection(path)
+            .add(data));
     }
     
     deleteBlogPost(slug: string) {
-        this.store.collection(
+        this.fireStore.collection(
             this.blogPostPath,
-            ref => ref.limit(1).where('slug', '==', slug)
+            ref => ref.limit(1)
+                .where('slug', '==', slug)
         )
             .get()
             .pipe(mergeMap(x => x.docs), take(1))
@@ -73,7 +102,7 @@ export class FirebaseService {
     }
     
     getInstagramList(limit?: number) {
-        return this.store.collection(
+        return this.fireStore.collection(
             this.instaPath,
             ref => ref.limit(limit ? limit : 999)
         )
@@ -85,14 +114,35 @@ export class FirebaseService {
             .pipe(
                 filter(x => x && x.exists),
                 map(x => x.id),
-                map(x => this.store.collection(path).doc(x)),
+                map(x => this.fireStore.collection(path)
+                    .doc(x)),
                 switchMap(x => x.update(newData))
             );
         
     }
     
+    private firebaseLogin() {
+        const persistence$ = fromPromise(this.fireAuth.auth.setPersistence(AuthPersistence.LOCAL))
+            .pipe(take(1));
+        const auth$ = fromPromise(this.fireAuth.auth.signInWithPopup(new GoogleAuthProvider()));
+        
+        concat(
+            persistence$.pipe(map(() => undefined)),
+            auth$
+        )
+            .pipe(
+                // filter(x => !!x),
+                // tap(x => console.log(x))
+            )
+            .subscribe(this.user$);
+    }
+    
+    private firebaseLogout() {
+        return fromPromise(this.fireAuth.auth.signOut());
+    }
+    
     private getSingleWithSlug(path: string, slug) {
-        return this.store.collection(
+        return this.fireStore.collection(
             path,
             ref => ref.limit(1)
                 .where('slug', '==', slug)
@@ -103,7 +153,7 @@ export class FirebaseService {
     }
     
     private getBlogList(limit?: number) {
-        return this.store.collection(
+        return this.fireStore.collection(
             this.blogPostPath,
             ref => ref.limit(limit ? limit : 999)
                 .where('public', '==', true)
