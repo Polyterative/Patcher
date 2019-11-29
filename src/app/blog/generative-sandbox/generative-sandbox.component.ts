470da59261a620c5dc8443d098359cb2dfe1b74a
vlady <vlady.internet@outlook.it>
2019-11-29 14:50:48 +0100

    test

--- src/app/blog/generative-sandbox/generative-sandbox.component.ts
+++ src/app/blog/generative-sandbox/generative-sandbox.component.ts
@@ -23,6 +23,8 @@ export class GenerativeSandboxComponent implements OnInit {
     private toggle = true;
     private p5: p5;
     
+    private canvasPadding = 100;
+    
     constructor() {
         console.log('Analog-constructed');
         window.onresize = this.onWindowResize;
@@ -42,7 +44,6 @@ export class GenerativeSandboxComponent implements OnInit {
                 this.color.next('#2a2a5a');
             });
         
-        
         interval(2000)
             .subscribe(x => {
                 this.color.next('#2d5a44');
@@ -54,7 +55,6 @@ export class GenerativeSandboxComponent implements OnInit {
         console.log('analog-destroy');
     }
     
-    private canvasPadding = 50;
     private onWindowResize = e => {
         this.p5.resizeCanvas(this.p5.windowWidth - this.canvasPadding, this.p5.windowHeight - this.canvasPadding);
     };
@@ -76,6 +76,7 @@ export class GenerativeSandboxComponent implements OnInit {
             p.angleMode(p.DEGREES);
             p.rectMode(p.CENTER);
             p.background(0);
+            p.frameRate(60);
         };
         p.center = {
             x: 0,
@@ -109,14 +110,25 @@ export class GenerativeSandboxComponent implements OnInit {
         
         // dail
         p.stroke(175);
-        p.arc(0, 0, 210, 210, 0, 360);
+        const clockRadius = 210;
+        p.arc(0, 0, clockRadius, clockRadius, 0, 360);
+        
+        p.stroke('#547dbb');
+        p.arc(0, 0, clockRadius + 20, clockRadius + 20, 20, 200);
+        p.stroke('#bb636b');
+        p.arc(0, 0, clockRadius + 20, clockRadius + 20, 210, 300);
+        
+        p.stroke('#bb3a49');
+        p.arc(0, 0, clockRadius - 20, clockRadius - 20, 20, 76);
+        p.stroke('#bb636b');
+        p.arc(0, 0, clockRadius - 20, clockRadius - 20, 110, 190);
         
         // second
         const sc_end = p.map(second % 60, 0, 60, 0, 360);
         
         p.push();
         p.rotate(sc_end);
-        p.stroke(255, 0, 0);
+        p.stroke('#4eaa8c');
         p.line(0, 0, 90, 0);
         p.pop();
         
@@ -124,7 +136,7 @@ export class GenerativeSandboxComponent implements OnInit {
         const mn_end = p.map(minute % 60, 0, 60, 0, 360);
         p.push();
         p.rotate(mn_end);
-        p.stroke(0, 230, 0);
+        p.stroke('#4ba6aa');
         p.line(0, 0, 70, 0);
         p.pop();
         
@@ -132,7 +144,7 @@ export class GenerativeSandboxComponent implements OnInit {
         const hr_end = p.map(hour % 12, 0, 12, 0, 360);
         p.push();
         p.rotate(hr_end);
-        p.stroke(0, 0, 230);
+        p.stroke('#7aaa56');
         p.line(0, 0, 50, 0);
         p.pop();
         
@@ -143,10 +155,10 @@ export class GenerativeSandboxComponent implements OnInit {
         
         p.pop();
         
-        // const clock = hour + ':' + minute + ':' + second;
-        // p.fill(255);
-        // p.noStroke();
-        // p.textSize(25);
-        // p.text(clock, 100, 50);
+        const clock = hour + ':' + minute + ':' + second;
+        p.fill(255);
+        p.noStroke();
+        p.textSize(25);
+        p.text(clock, 100, 50);
     }
 }
