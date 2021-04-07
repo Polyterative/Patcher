949060ba7c7e5d505ca368eda47629dba679bf08
Vlady Yakovenko <vlady.yakovenko@oltresolutions.it>
2021-04-07 15:18:00 +0200

    progress

--- src/app/features/patchBuilder/patch-builder.module.ts
+++ src/app/features/patchBuilder/patch-builder.module.ts
@@ -1,17 +1,25 @@
 import { CommonModule }              from '@angular/common';
 import { NgModule }                  from '@angular/core';
+import { RouterModule }              from '@angular/router';
 import { PatchBuilderRootComponent } from './patch-builder-root.component';
 
 
 @NgModule({
-  declarations: [
-    PatchBuilderRootComponent
-  ],
-  imports: [
-    CommonModule
-  ],
-  exports: [
-    PatchBuilderRootComponent
-  ]
+    declarations: [
+        PatchBuilderRootComponent
+    ],
+    imports:      [
+        CommonModule,
+        RouterModule.forRoot([
+            {
+                path:      'builder',
+                component: PatchBuilderRootComponent
+                // pathMatch: 'full'
+            }
+        ])
+    ],
+    exports:      [
+        PatchBuilderRootComponent
+    ]
 })
-export class PatchBuilderModule { }
+export class PatchBuilderModule {}
