4ce0ab7526c187ff2003c2623bb6a05f6bbdb6ee
Vlady Yakovenko <vlady.yakovenko@oltresolutions.it>
2021-04-07 15:08:05 +0200

    progress

---
+++ src/app/features/toolbar/toolbar.module.ts
@@ -0,0 +1,41 @@
+﻿import { CommonModule }        from '@angular/common';
+import { NgModule }            from '@angular/core';
+import {
+    FlexLayoutModule,
+    FlexModule
+}                              from '@angular/flex-layout';
+import { MatButtonModule }     from '@angular/material/button';
+import { MatIconModule }       from '@angular/material/icon';
+import { MatMenuModule }       from '@angular/material/menu';
+import { MatToolbarModule }    from '@angular/material/toolbar';
+import { RouterModule }        from '@angular/router';
+import { BrandLogoModule }     from '../../shared-interproject/components/@visual/brand-logo/brand-logo.module';
+import { ScreenWrapperModule } from '../../shared-interproject/components/@visual/screen-wrapper/screen-wrapper.module';
+import { ToolbarComponent }    from './toolbar.component';
+import { ToolbarService }      from './toolbar.service';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    FlexModule,
+    MatMenuModule,
+    FlexLayoutModule,
+    MatToolbarModule,
+    BrandLogoModule,
+    RouterModule,
+    ScreenWrapperModule
+  ],
+  declarations: [
+    ToolbarComponent
+  ],
+  exports:      [
+    ToolbarComponent
+  ],
+  providers:    [
+    ToolbarService
+  ]
+})
+export class ToolbarModule {}
