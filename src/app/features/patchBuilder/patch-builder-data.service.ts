947633e9a8d31ebf938812716551b8bd5e7993d1
Vlady Yakovenko <vlady.yakovenko@oltresolutions.it>
2021-04-07 16:32:28 +0200

    progress

--- src/app/features/patchBuilder/patch-builder-data.service.ts
+++ src/app/features/patchBuilder/patch-builder-data.service.ts
@@ -5,6 +5,8 @@ import {
 }                     from 'rxjs';
 import {
     Connection,
+    EuroModule,
+    modules,
     Patch
 }                     from './models/models';
 
@@ -13,20 +15,42 @@ export class PatchBuilderDataService {
     generate$ = new Subject();
     generated$ = new BehaviorSubject<Patch | undefined>(undefined);
     
-    
     constructor() {
         this.generate$.subscribe(x => {
-            let toReturn: Connection[] = [];
+            const toReturn: Connection[] = [];
             
-            for (let i = 0; i < 5; i++) {
+            for (let i = 0; i < 10; i++) {
+                
+                const A: EuroModule = modules[this.randomIntFromInterval(modules.length - 1)];
+                const B: EuroModule = modules[this.randomIntFromInterval(modules.length - 1)];
+                
+                let Aouts = A.outs;
+                const Aid: number = this.randomIntFromInterval(Aouts.length - 1);
+                
+                let bINs = B.ins;
+                const bID: number = this.randomIntFromInterval(bINs.length - 1);
+                
+                
                 toReturn.push({
-                    from: '',
-                    to:   ''
+                    from: `${ A.name } ${ Aouts[Aid]?.title }`,
+                    to:   `${ B.name } ${ bINs[bID]?.title }`
                 });
+                
+                console.log([
+                    A,
+                    B,
+                    Aid,
+                    bID
+                ]);
+                // debugger
+                
             }
             
             this.generated$.next({connections: toReturn});
         });
     }
+    
+    randomIntFromInterval(max, min = 0) { // min and max included 
+        return Math.floor(Math.random() * (max - min + 1) + min);
+    }
 }
-
