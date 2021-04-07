b2d0956ee0ea85c29179e1cbecdd421aa9ef38b4
Vlady Yakovenko <vlady.yakovenko@oltresolutions.it>
2021-04-07 14:57:00 +0200

    progress

---
+++ src/app/features/home/home.module.ts
@@ -0,0 +1,44 @@
+import { CommonModule }          from '@angular/common';
+import { NgModule }              from '@angular/core';
+import { FlexLayoutModule }      from '@angular/flex-layout';
+import { MatButtonModule }       from '@angular/material/button';
+import { MatCardModule }         from '@angular/material/card';
+import { MatToolbarModule }      from '@angular/material/toolbar';
+import { RouterModule }          from '@angular/router';
+import { EmptyStateModule }      from '../../shared-interproject/components/@smart/empty-state/empty-state.module';
+import { ListLinkRouterModule }  from '../../shared-interproject/components/@smart/list-link-router/list-link-router.module';
+import { UserDataHandlerModule } from '../../shared-interproject/components/@smart/user-data-handler/user-data-handler.module';
+import { HeroLinkModule }        from '../../shared-interproject/components/@visual/hero-link/hero-link.module';
+import { ScreenWrapperModule }   from '../../shared-interproject/components/@visual/screen-wrapper/screen-wrapper.module';
+import { HomeComponent }         from './home.component';
+
+@NgModule({
+  declarations: [HomeComponent],
+  exports:      [HomeComponent],
+    imports: [
+        CommonModule,
+        FlexLayoutModule,
+        MatButtonModule,
+        MatCardModule,
+        RouterModule.forRoot([
+            {
+                path:       '',
+                redirectTo: 'home',
+                pathMatch:  'full'
+            },
+            {
+                path:      'home',
+                component: HomeComponent
+            }
+        ]),
+        // EmptyStateModule,
+        MatCardModule,
+        ScreenWrapperModule,
+        ListLinkRouterModule,
+        HeroLinkModule,
+        UserDataHandlerModule,
+        MatToolbarModule,
+        EmptyStateModule
+    ]
+})
+export class HomeModule {}
