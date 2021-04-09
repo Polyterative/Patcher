37557eebc579113f4374a21b74b1f3dddc0d1338
Vlady Yakovenko <vlady.yakovenko@oltresolutions.it>
2021-04-09 17:46:29 +0200

    progress

---
+++ src/app/features/patchBuilder/moduleBrowser/module-browser-module-detail-view-root/module-browser-module-detail-view-root.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+
+@Component({
+  selector: 'app-module-browser-module-detail-view-root',
+  templateUrl: './module-browser-module-detail-view-root.component.html',
+  styleUrls: ['./module-browser-module-detail-view-root.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class ModuleBrowserModuleDetailViewRootComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
