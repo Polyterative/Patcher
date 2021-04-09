7412493072a918ad9e04749d8d7b590bd22d48ff
Vlady Yakovenko <vlady.yakovenko@oltresolutions.it>
2021-04-09 17:34:10 +0200

    progress

--- src/app/features/patchBuilder/moduleBrowser/module-browser.module.ts
+++ src/app/features/patchBuilder/moduleBrowser/module-browser.module.ts
@@ -1,11 +1,15 @@
 import { DragDropModule }               from '@angular/cdk/drag-drop';
+import { ScrollingModule }              from '@angular/cdk/scrolling';
 import { CommonModule }                 from '@angular/common';
 import { NgModule }                     from '@angular/core';
 import { FlexLayoutModule }             from '@angular/flex-layout';
 import { MatCardModule }                from '@angular/material/card';
 import { MatChipsModule }               from '@angular/material/chips';
+import { RouterModule }                 from '@angular/router';
 import { HeroContentCardModule }        from '../../../shared-interproject/components/@visual/hero-content-card/hero-content-card.module';
 import { LabelValueShowcaseModule }     from '../../../shared-interproject/components/@visual/label-value-showcase/label-value-showcase.module';
+import { ScreenWrapperModule }          from '../../../shared-interproject/components/@visual/screen-wrapper/screen-wrapper.module';
+import { PatchBuilderRootComponent }    from '../patch-builder-root.component';
 import { ModuleBrowserDataService }     from './module-browser-data.service';
 import { ModuleBrowserModuleComponent } from './module-browser-module/module-browser-module.component';
 import { ModuleBrowserRootComponent }   from './module-browser-root/module-browser-root.component';
@@ -19,12 +23,21 @@ import { ModuleBrowserRootComponent }   from './module-browser-root/module-brows
     providers:    [ModuleBrowserDataService],
     imports: [
         CommonModule,
+        RouterModule.forRoot([
+            {
+                path:      'browser',
+                component: ModuleBrowserRootComponent
+                // pathMatch: 'full'
+            }
+        ]),
         FlexLayoutModule,
         MatCardModule,
         HeroContentCardModule,
         MatChipsModule,
         DragDropModule,
-        LabelValueShowcaseModule
+        LabelValueShowcaseModule,
+        ScrollingModule,
+        ScreenWrapperModule
     ],
     exports:      [
         ModuleBrowserRootComponent,
