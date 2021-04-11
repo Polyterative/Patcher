6cb5f3b71bf99bbcecdc2257e863268fb1a2e964
Vlady Yakovenko <vlady.yakovenko@oltresolutions.it>
2021-04-11 20:53:03 +0200

    progress

--- src/app/features/backbone/backbone.module.ts
+++ src/app/features/backbone/backbone.module.ts
@@ -1,10 +1,12 @@
 import { HttpClientModule }       from '@angular/common/http';
 import { NgModule }               from '@angular/core';
+import { FlexLayoutModule }       from '@angular/flex-layout';
 import { AppStateService }        from '../../shared-interproject/app-state.service';
 import { UserDataHandlerService } from '../../shared-interproject/components/@smart/user-data-handler/user-data-handler.service';
 import { NotFoundModule }         from './404/not-found.module';
 import { CommonSidebarComponent } from './common-sidebar/common-sidebar.component';
 import { FeedbackBoxModule }      from './feedback-box/feedback-box.module';
+import { FooterComponent }        from './footer/footer.component';
 import { HomeModule }             from './home/home.module';
 import { ToolbarModule }          from './toolbar/toolbar.module';
 
@@ -18,13 +20,17 @@ import { ToolbarModule }          from './toolbar/toolbar.module';
         HomeModule,
         ToolbarModule,
         FeedbackBoxModule,
-        NotFoundModule //keep as last (for routes)
+        NotFoundModule,
+        FlexLayoutModule
+        //keep as last (for routes)
     ],
     declarations: [
-        CommonSidebarComponent
+        CommonSidebarComponent,
+        FooterComponent
     ],
     exports:      [
-        CommonSidebarComponent
+        CommonSidebarComponent,
+        FooterComponent
     ]
 })
 export class BackboneModule {}
