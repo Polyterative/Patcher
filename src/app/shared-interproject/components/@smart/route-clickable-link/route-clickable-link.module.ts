9b286197b815c22dbd854611d524969e7cdec267
Vlady Yakovenko <vlady.yakovenko@oltresolutions.it>
2021-04-24 13:31:48 +0200

    UI

--- src/app/shared-interproject/components/@smart/route-clickable-link/route-clickable-link.module.ts
+++ src/app/shared-interproject/components/@smart/route-clickable-link/route-clickable-link.module.ts
@@ -5,25 +5,27 @@ import { MatButtonModule }             from '@angular/material/button';
 import { MatIconModule }               from '@angular/material/icon';
 import { MatListModule }               from '@angular/material/list';
 import { MatMenuModule }               from '@angular/material/menu';
+import { MatTooltipModule }            from '@angular/material/tooltip';
 import { RouterModule }                from '@angular/router';
 import { RouteClickableLinkComponent } from './route-clickable-link/route-clickable-link.component';
 
 
 @NgModule({
-    declarations: [
-        RouteClickableLinkComponent
-    ],
-    imports:      [
-        CommonModule,
-        MatIconModule,
-        MatMenuModule,
-        RouterModule,
-        FlexLayoutModule,
-        MatListModule,
-        MatButtonModule
-    ],
-    exports:      [
-        RouteClickableLinkComponent
-    ]
+  declarations: [
+    RouteClickableLinkComponent
+  ],
+  imports:      [
+    CommonModule,
+    MatIconModule,
+    MatMenuModule,
+    RouterModule,
+    FlexLayoutModule,
+    MatListModule,
+    MatButtonModule,
+    MatTooltipModule
+  ],
+  exports:      [
+    RouteClickableLinkComponent
+  ]
 })
 export class RouteClickableLinkModule {}
--- src/app/shared-interproject/components/@visual/hero-content-card/hero-content-card.component.ts
+++ src/app/shared-interproject/components/@visual/hero-content-card/hero-content-card.component.ts
@@ -1,10 +1,4 @@
 import {
-  animate,
-  style,
-  transition,
-  trigger
-} from '@angular/animations';
-import {
   ChangeDetectionStrategy,
   Component,
   Input
@@ -17,28 +11,7 @@ import {
   selector:        'app-hero-content-card',
   templateUrl:     './hero-content-card.component.html',
   styleUrls:       ['./hero-content-card.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush,
-  animations:      [
-    trigger('fadeInOut', [
-      transition(':enter', [   // :enter is alias to 'void => *'
-        style({
-          opacity: 0,
-          // height:  0.01
-        }),
-        animate('225ms cubic-bezier(0.0, 0.0, 0.2, 1)'),
-        style(
-          {opacity: 1}
-        )
-      ]),
-      transition(':leave', [   // :leave is alias to '* => void'
-        animate('75ms cubic-bezier(0.4, 0.0, 1, 1)'),
-        style(
-          {opacity: 0}
-        )
-      ])
-    ])
-  ]
-
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class HeroContentCardComponent {
   @Input()
