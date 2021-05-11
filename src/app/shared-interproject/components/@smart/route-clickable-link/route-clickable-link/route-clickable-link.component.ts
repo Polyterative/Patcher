aeeccf64a5b098e3aac6ee4f47584302117f207b
Vlady Yakovenko <vlady.yakovenko@oltresolutions.it>
2021-05-11 22:10:11 +0200

    UI

--- src/app/shared-interproject/components/@smart/route-clickable-link/route-clickable-link/route-clickable-link.component.ts
+++ src/app/shared-interproject/components/@smart/route-clickable-link/route-clickable-link/route-clickable-link.component.ts
@@ -1,32 +1,51 @@
 import {
-    ChangeDetectionStrategy,
-    Component,
-    Input,
-    OnInit
+  ChangeDetectionStrategy,
+  Component,
+  Input,
+  OnInit
 }                          from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
+import { AppStateService } from 'src/app/shared-interproject/app-state.service';
 
 @Component({
-    selector:        'app-route-clickable-link-list',
-    templateUrl:     './route-clickable-link.component.html',
-    styleUrls:       ['./route-clickable-link.component.scss'],
-    changeDetection: ChangeDetectionStrategy.OnPush
+  selector:        'app-route-clickable-link-list',
+  templateUrl:     './route-clickable-link.component.html',
+  styleUrls:       ['./route-clickable-link.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class RouteClickableLinkComponent implements OnInit {
-    @Input()
-    public readonly data$ = new BehaviorSubject<RouteClickableLink[]>([]);
+  @Input()
+  public readonly data$ = new BehaviorSubject<RouteClickableLink[]>([]);
+  
+  
+  constructor(
+    public appState: AppStateService
+  ) {
     
-    ngOnInit(): void {
-    }
+  }
+  
+  ngOnInit(): void {
+  }
+  
+  doNothing(): any {
     
-    doNothing(): any {
-    
-    }
+  }
+}
+
+interface RouteClickableLinkMenuItem {
+  label: string,
+}
+
+interface RouteClickableLinkMenu {
+  label: string,
+  items: RouteClickableLinkMenuItem[]
 }
 
 export interface RouteClickableLink {
-    label: string,
-    route: string,
-    icon?: string;
-    disabled: boolean;
+  label: string,
+  route: string,
+  disabled: boolean;
+  // menu?: RouteClickableLinkMenu;
+  icon?: string;
+  style?: { [param: string]: any };
 }
