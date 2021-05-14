6cd4acf85e0107d6346ffa3f01bad7541db53837
Vlady Yakovenko <vlady.yakovenko@oltresolutions.it>
2021-05-14 13:14:13 +0200

    progress

--- src/app/shared-interproject/components/@visual/hero-content-card/hero-content-card.component.ts
+++ src/app/shared-interproject/components/@visual/hero-content-card/hero-content-card.component.ts
@@ -1,4 +1,10 @@
 import {
+  animate,
+  style,
+  transition,
+  trigger
+} from '@angular/animations';
+import {
   ChangeDetectionStrategy,
   Component,
   Input
@@ -11,20 +17,35 @@ import {
   selector:        'lib-hero-content-card',
   templateUrl:     './hero-content-card.component.html',
   styleUrls:       ['./hero-content-card.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  animations:      [
+    trigger('fadeInOut', [
+      transition(':enter', [   // :enter is alias to 'void => *'
+        style({
+          opacity: 0
+          // height:  0.01
+        }),
+        animate('225ms cubic-bezier(0.0, 0.0, 0.2, 1)'),
+        style(
+          {opacity: 1}
+        )
+      ]),
+      transition(':leave', [   // :leave is alias to '* => void'
+        animate('75ms cubic-bezier(0.4, 0.0, 1, 1)'),
+        style(
+          {opacity: 0}
+        )
+      ])
+    ])
+  ]
 })
 export class HeroContentCardComponent {
-  @Input()
-  titleBig: string = '';
-  @Input()
-  titleNormal: string = '';
-  @Input()
-  top = false;
-  @Input()
-  bottom = false;
-  @Input()
-  description: string = '';
-  @Input()
-  icon: string;
+  @Input() titleBig: string = '';
+  @Input() titleNormal: string = '';
+  @Input() top = false;
+  @Input() bottom = false;
+  @Input() description: string = '';
+  @Input() hideDescription = false;
+  @Input() icon: string;
   
 }
