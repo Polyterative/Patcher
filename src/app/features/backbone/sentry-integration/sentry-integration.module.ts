01bb17320de3637f243cc737baea1889a1dbb570
Vlady Yakovenko <vlady.yakovenko@oltresolutions.it>
2021-05-24 19:01:50 +0200

    added sentry

---
+++ src/app/features/backbone/sentry-integration/sentry-integration.module.ts
@@ -0,0 +1,36 @@
+import { CommonModule } from '@angular/common';
+import {
+  APP_INITIALIZER,
+  ErrorHandler,
+  NgModule
+}                       from '@angular/core';
+import { Router }       from '@angular/router';
+import * as Sentry      from '@sentry/angular';
+
+@NgModule({
+  declarations: [],
+  providers:    [
+    {
+      provide:  ErrorHandler,
+      useValue: Sentry.createErrorHandler({
+        showDialog: true
+      })
+    },
+    {
+      provide: Sentry.TraceService,
+      deps:    [Router]
+    },
+    {
+      provide:    APP_INITIALIZER,
+      useFactory: () => () => {},
+      deps:       [Sentry.TraceService],
+      multi:      true
+    }
+  ],
+  imports:      [
+    CommonModule
+  ]
+})
+export class SentryIntegrationModule {
+  
+}
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,48 @@
 import { enableProdMode }         from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import * as Sentry                from '@sentry/angular';
+import { Integrations }           from '@sentry/tracing';
 
 import { AppModule }   from './app/app.module';
 import { environment } from './environments/environment';
 
+let sentry;
+
 if (environment.production) {
-    enableProdMode();
+  enableProdMode();
+  
+  sentry = {
+    dsn:              'https://57dc8f0b1ad240f3afa61628b8351aae@o718439.ingest.sentry.io/5780783',
+    integrations:     [
+      new Integrations.BrowserTracing({
+        tracingOrigins:         [
+          'localhost',
+          'https://patcher.xyz'
+        ],
+        routingInstrumentation: Sentry.routingInstrumentation
+      })
+    ],
+    tracesSampleRate: 1
+  };
+} else {
+  sentry = {
+    dsn:              'https://57dc8f0b1ad240f3afa61628b8351aae@o718439.ingest.sentry.io/5780783',
+    integrations:     [
+      new Integrations.BrowserTracing({
+        tracingOrigins:         [
+          'localhost',
+          'https://patcher.xyz'
+        ],
+        routingInstrumentation: Sentry.routingInstrumentation
+      })
+    ],
+    tracesSampleRate: 0
+  };
+  
 }
+
+
+Sentry.init(sentry);
 
 platformBrowserDynamic()
   .bootstrapModule(AppModule)
