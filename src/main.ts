a84fd7fc02ffdb0db778f9010d1a6b7c96bf2660
Vlady Yakovenko <vlady.yakovenko@oltresolutions.it>
2021-05-24 19:20:35 +0200

    added sentry

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import * as Sentry                from '@sentry/angular';
 import { Integrations }           from '@sentry/tracing';
 
 import { AppModule }   from './app/app.module';
+import build           from './build';
 import { environment } from './environments/environment';
 
 let sentry;
@@ -13,6 +14,7 @@ if (environment.production) {
   
   sentry = {
     environment:      'production',
+    release:          'patcher@' + build.version,
     dsn:              'https://57dc8f0b1ad240f3afa61628b8351aae@o718439.ingest.sentry.io/5780783',
     integrations:     [
       new Integrations.BrowserTracing({
@@ -28,6 +30,7 @@ if (environment.production) {
 } else {
   sentry = {
     environment:      'development',
+    release:          'patcher@' + build.version,
     dsn:              'https://57dc8f0b1ad240f3afa61628b8351aae@o718439.ingest.sentry.io/5780783',
     integrations:     [
       new Integrations.BrowserTracing({
