95c60f05c8dd58cd12baba250b34f019680f4327
Vlady Yakovenko <vlady.y@live.it>
2021-09-16 23:04:03 +0200

    ui
    ui
    
    ui
    
    ui
    
    ui
    
    ui
    
    ui
    
    ui

--- src/app/components/shared-atoms/shared-atoms.module.ts
+++ src/app/components/shared-atoms/shared-atoms.module.ts
@@ -2,6 +2,7 @@ import { CommonModule }                   from '@angular/common';
 import { NgModule }                       from '@angular/core';
 import { FlexLayoutModule }               from '@angular/flex-layout';
 import { MatCardModule }                  from '@angular/material/card';
+import { MatIconModule }                  from '@angular/material/icon';
 import { TimeagoModule }                  from 'ngx-timeago';
 import { HeroInfoBoxModule }              from 'src/app/shared-interproject/components/@visual/hero-info-box/hero-info-box.module';
 import { CommentsModule }                 from './comments/comments.module';
@@ -21,7 +22,8 @@ import { TimestampsRelativeComponent }    from './timestamps-relative/timestamps
     TimeagoModule.forChild(),
     MatCardModule,
     HeroInfoBoxModule,
-    CommentsModule
+    CommentsModule,
+    MatIconModule
   ],
   exports:      [
     TimestampsRelativeComponent,
