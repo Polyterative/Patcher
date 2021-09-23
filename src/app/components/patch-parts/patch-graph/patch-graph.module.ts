ad069e33deae84810b0e078a6ad37136fe2772bb
Vlady Yakovenko <vladyslav.yakovenko@kantar.com>
2021-09-23 12:40:19 +0200

    progress

--- src/app/components/patch-parts/patch-graph/patch-graph.module.ts
+++ src/app/components/patch-parts/patch-graph/patch-graph.module.ts
@@ -1,5 +1,6 @@
 import { NgModule }            from '@angular/core';
 import { CommonModule }        from '@angular/common';
+import { FlexLayoutModule }    from '@angular/flex-layout';
 import { NgxGraphModule }      from '@swimlane/ngx-graph';
 import { PatchGraphComponent } from './patch-graph/patch-graph.component';
 
@@ -11,7 +12,8 @@ import { PatchGraphComponent } from './patch-graph/patch-graph.component';
   ],
   imports: [
     CommonModule,
-    NgxGraphModule
+    NgxGraphModule,
+    FlexLayoutModule
   ],
   exports: [
     PatchGraphComponent
--- src/app/components/patch-parts/patch-graph/patch-graph/patch-graph.component.ts
+++ src/app/components/patch-parts/patch-graph/patch-graph/patch-graph.component.ts
@@ -1,16 +1,170 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  OnInit
+}                  from '@angular/core';
+import {
+  ClusterNode,
+  Edge,
+  Layout,
+  Node
+}                  from '@swimlane/ngx-graph';
+import { Subject } from 'rxjs';
 
 @Component({
-  selector: 'app-patch-graph',
-  templateUrl: './patch-graph.component.html',
-  styleUrls: ['./patch-graph.component.scss'],
+  selector:        'app-patch-graph',
+  templateUrl:     './patch-graph.component.html',
+  styleUrls:       ['./patch-graph.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class PatchGraphComponent implements OnInit {
-
-  ngOnInit(): void {
+  
+  nodes: Node[] = [
+    {
+      id:    'first',
+      label: 'A'
+    },
+    {
+      id:    'second',
+      label: 'B'
+    },
+    {
+      id:    'c1',
+      label: 'C1'
+    },
+    {
+      id:    'c2',
+      label: 'C2'
+    }
+  ];
+  
+  clusters: ClusterNode[] = [
+    {
+      id:           'third',
+      label:        'C',
+      childNodeIds: [
+        'c1',
+        'c2'
+      ]
+    }
+  ];
+  
+  links: Edge[] = [
+    {
+      id:     'a',
+      source: 'first',
+      target: 'second',
+      label:  'is parent of'
+    },
+    {
+      id:     'b',
+      source: 'first',
+      target: 'c1',
+      label:  'custom label'
+    },
+    {
+      id:     'c',
+      source: 'first',
+      target: 'c1',
+      label:  'custom label'
+    },
+    {
+      id:     'd',
+      source: 'first',
+      target: 'c2',
+      label:  'custom label'
+    }
+  ];
+  
+  layout: String | Layout = 'dagreCluster';
+  layouts: any[] = [
+    {
+      label: 'Dagre',
+      value: 'dagre'
+    },
+    {
+      label:       'Dagre Cluster',
+      value:       'dagreCluster',
+      isClustered: true
+    },
+    {
+      label:       'Cola Force Directed',
+      value:       'colaForceDirected',
+      isClustered: true
+    },
+    {
+      label: 'D3 Force Directed',
+      value: 'd3ForceDirected'
+    }
+  ];
+  
+  // line interpolation
+  curveType = 'Bundle';
+  interpolationTypes = [
+    'Bundle',
+    'Cardinal',
+    'Catmull Rom',
+    'Linear',
+    'Monotone X',
+    'Monotone Y',
+    'Natural',
+    'Step',
+    'Step After',
+    'Step Before'
+  ];
+  
+  draggingEnabled = true;
+  panningEnabled = true;
+  zoomEnabled = true;
+  
+  zoomSpeed = 0.1;
+  minZoomLevel = 0.1;
+  maxZoomLevel = 4;
+  panOnZoom = true;
+  
+  autoZoom = false;
+  autoCenter = false;
+  
+  update$: Subject<boolean> = new Subject();
+  center$: Subject<boolean> = new Subject();
+  zoomToFit$: Subject<boolean> = new Subject();
+  
+  ngOnInit() {
+    // this.setInterpolationType(this.curveType);
   }
   
+  // setInterpolationType(curveType) {
+  //   this.curveType = curveType;
+  //   if (curveType === 'Bundle') {
+  //     this.curve = shape.curveBundle.beta(1);
+  //   }
+  //   if (curveType === 'Cardinal') {
+  //     this.curve = shape.curveCardinal;
+  //   }
+  //   if (curveType === 'Catmull Rom') {
+  //     this.curve = shape.curveCatmullRom;
+  //   }
+  //   if (curveType === 'Linear') {
+  //     this.curve = shape.curveLinear;
+  //   }
+  //   if (curveType === 'Monotone X') {
+  //     this.curve = shape.curveMonotoneX;
+  //   }
+  //   if (curveType === 'Monotone Y') {
+  //     this.curve = shape.curveMonotoneY;
+  //   }
+  //   if (curveType === 'Natural') {
+  //     this.curve = shape.curveNatural;
+  //   }
+  //   if (curveType === 'Step') {
+  //     this.curve = shape.curveStep;
+  //   }
+  //   if (curveType === 'Step After') {
+  //     this.curve = shape.curveStepAfter;
+  //   }
+  //   if (curveType === 'Step Before') {
+  //     this.curve = shape.curveStepBefore;
+  //   }
+  // }
   
-
 }
