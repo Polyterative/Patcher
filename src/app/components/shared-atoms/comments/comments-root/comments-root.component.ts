9cd7909ceba486b60c8482993d7ae51367b2d49a
Vlady Yakovenko <vladyslav.yakovenko@kantar.com>
2021-09-30 12:52:50 +0200

    refactor models.ts

--- src/app/components/shared-atoms/comments/comments-root/comments-root.component.ts
+++ src/app/components/shared-atoms/comments/comments-root/comments-root.component.ts
@@ -21,5 +21,4 @@ export class CommentsRootComponent {
     public appState: AppStateService
   ) { }
   
-  
 }
--- src/app/models/models.ts
+++ src/app/models/models.ts
@@ -1,6 +1,9 @@
-
-
 export interface Timestamped {
   created: string;
   updated: string;
+}
+
+
+export interface DbComment {
+
 }
