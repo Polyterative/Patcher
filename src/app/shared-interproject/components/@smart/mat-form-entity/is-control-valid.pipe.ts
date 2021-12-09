215f7bbd47c4b447c0db3ce60674846a19dc1aaa
Vlady Yakovenko <vladyslav.yakovenko@kantar.com>
2021-12-09 10:07:46 +0100

    lib mat form entity up

--- src/app/shared-interproject/components/@smart/mat-form-entity/is-control-valid.pipe.ts
+++ src/app/shared-interproject/components/@smart/mat-form-entity/is-control-valid.pipe.ts
@@ -1,13 +1,6 @@
-import {
-  ChangeDetectorRef,
-  Pipe,
-  PipeTransform
-}                    from '@angular/core';
-import {
-  FormControl,
-  FormGroup
-}                    from '@angular/forms';
-import { Subject }   from 'rxjs';
+import { ChangeDetectorRef, Pipe, PipeTransform } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Subject } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
 
 type LocalType = FormControl | FormGroup;
@@ -19,42 +12,42 @@ type LocalType = FormControl | FormGroup;
 export class FormValidPipe implements PipeTransform {
   valid = false;
   subscribed = false;
-  
+
   protected destroyEvent$: Subject<void> = new Subject();
-  
+
   constructor(public changeDetection: ChangeDetectorRef) {}
-  
+
   transform(control: LocalType): boolean {
-    
+
     if (!this.subscribed) {
       this.subscribe(control);
     }
-    
+
     return this.valid;
   }
-  
+
   private subscribe(control: LocalType): void {
     control.valueChanges
            .pipe(
              takeUntil(this.destroyEvent$)
            )
-           .subscribe(value => {
+           .subscribe(_ => {
              this.updateResult(control);
            });
-    
+
     this.subscribed = true;
-    
+
     this.updateResult(control);
   }
-  
+
   private updateResult(control: LocalType): void {
     this.valid = control.valid;
     this.changeDetection.detectChanges();
   }
-  
+
   ngOnDestroy(): void {
     this.destroyEvent$.next();
     this.destroyEvent$.complete();
-    
+
   }
 }
