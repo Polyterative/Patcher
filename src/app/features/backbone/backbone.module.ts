8fd49c9ef5361584f54e115a1c6d2f19237640fd
Vlady Yakovenko <vlady.y@live.it>
2021-12-28 12:43:53 +0100

    home

--- src/app/features/backbone/backbone.module.ts
+++ src/app/features/backbone/backbone.module.ts
@@ -1,24 +1,25 @@
-import { CommonModule }            from '@angular/common';
-import { HttpClientModule }        from '@angular/common/http';
-import { NgModule }                from '@angular/core';
-import { FlexLayoutModule }        from '@angular/flex-layout';
-import { MatCardModule }           from '@angular/material/card';
-import { TimeagoModule }           from 'ngx-timeago';
-import { UserManagementModule }    from 'src/app/features/backbone/user-management/user-management.module';
-import { AppStateService }         from '../../shared-interproject/app-state.service';
-import { DevOnlyWindowModule }     from '../../shared-interproject/components/@smart/dev-only-window/dev-only-window.module';
-import { UserDataHandlerService }  from '../../shared-interproject/components/@smart/user-data-handler/user-data-handler.service';
-import { HeroInfoBoxModule }       from '../../shared-interproject/components/@visual/hero-info-box/hero-info-box.module';
-import { HeroInfoBoxService }      from '../../shared-interproject/components/@visual/hero-info-box/hero-info-box.service';
-import { BuildInfoComponent }      from './build-info/build-info.component';
-import { CommonSidebarComponent }  from './common-sidebar/common-sidebar.component';
-import { DiscordWidgetComponent }  from './discord-widget/discord-widget.component';
-import { FeedbackBoxModule }       from './feedback-box/feedback-box.module';
-import { FooterComponent }         from './footer/footer.component';
-import { HomeModule }              from './home/home.module';
-import { LoginModule }             from './login/login.module';
-import { SentryIntegrationModule } from './sentry-integration/sentry-integration.module';
-import { ToolbarModule }           from './toolbar/toolbar.module';
+import { CommonModule }             from '@angular/common';
+import { HttpClientModule }         from '@angular/common/http';
+import { NgModule }                 from '@angular/core';
+import { FlexLayoutModule }         from '@angular/flex-layout';
+import { MatCardModule }            from '@angular/material/card';
+import { TimeagoModule }            from 'ngx-timeago';
+import { UserManagementModule }     from 'src/app/features/backbone/user-management/user-management.module';
+import { AppStateService }          from '../../shared-interproject/app-state.service';
+import { DevOnlyWindowModule }      from '../../shared-interproject/components/@smart/dev-only-window/dev-only-window.module';
+import { UserDataHandlerService }   from '../../shared-interproject/components/@smart/user-data-handler/user-data-handler.service';
+import { HeroInfoBoxModule }        from '../../shared-interproject/components/@visual/hero-info-box/hero-info-box.module';
+import { HeroInfoBoxService }       from '../../shared-interproject/components/@visual/hero-info-box/hero-info-box.service';
+import { LabelValueShowcaseModule } from '../../shared-interproject/components/@visual/label-value-showcase/label-value-showcase.module';
+import { BuildInfoComponent }       from './build-info/build-info.component';
+import { CommonSidebarComponent }   from './common-sidebar/common-sidebar.component';
+import { DiscordWidgetComponent }   from './discord-widget/discord-widget.component';
+import { FeedbackBoxModule }        from './feedback-box/feedback-box.module';
+import { FooterComponent }          from './footer/footer.component';
+import { HomeModule }               from './home/home.module';
+import { LoginModule }              from './login/login.module';
+import { SentryIntegrationModule }  from './sentry-integration/sentry-integration.module';
+import { ToolbarModule }            from './toolbar/toolbar.module';
 
 @NgModule({
   providers:    [
@@ -39,7 +40,8 @@ import { ToolbarModule }           from './toolbar/toolbar.module';
     MatCardModule,
     DevOnlyWindowModule,
     CommonModule,
-    TimeagoModule
+    TimeagoModule,
+    LabelValueShowcaseModule
     // NotFoundModule//keep as last (for routes)
   ],
   declarations: [
--- src/app/features/backbone/feedback-box/feedback-box.module.ts
+++ src/app/features/backbone/feedback-box/feedback-box.module.ts
@@ -1,22 +1,24 @@
-import { CommonModule }         from '@angular/common';
-import { NgModule }             from '@angular/core';
-import { FlexLayoutModule }     from '@angular/flex-layout';
-import { MatCardModule }        from '@angular/material/card';
-import { ScreenWrapperModule }  from '../../../shared-interproject/components/@visual/screen-wrapper/screen-wrapper.module';
-import { FeedbackBoxComponent } from './feedback-box.component';
+import { CommonModule }             from '@angular/common';
+import { NgModule }                 from '@angular/core';
+import { FlexLayoutModule }         from '@angular/flex-layout';
+import { MatCardModule }            from '@angular/material/card';
+import { LabelValueShowcaseModule } from '../../../shared-interproject/components/@visual/label-value-showcase/label-value-showcase.module';
+import { ScreenWrapperModule }      from '../../../shared-interproject/components/@visual/screen-wrapper/screen-wrapper.module';
+import { FeedbackBoxComponent }     from './feedback-box.component';
 
 
 @NgModule({
   declarations: [
     FeedbackBoxComponent
   ],
-    imports: [
-        CommonModule,
-        ScreenWrapperModule,
-        MatCardModule,
-        FlexLayoutModule
-    ],
-  exports: [
+  imports:      [
+    CommonModule,
+    ScreenWrapperModule,
+    MatCardModule,
+    FlexLayoutModule,
+    LabelValueShowcaseModule
+  ],
+  exports:      [
     FeedbackBoxComponent
   ]
 })
