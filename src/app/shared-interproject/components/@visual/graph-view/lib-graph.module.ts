050122a6f29814ad9ff2119a48aa9849417cd3d6
Vlady Yakovenko <vlady.y@live.it>
2022-01-17 22:12:32 +0100

    sigma.js

--- src/app/shared-interproject/components/@visual/graph-view/lib-graph.module.ts
+++ src/app/shared-interproject/components/@visual/graph-view/lib-graph.module.ts
@@ -1,9 +1,7 @@
 import { CommonModule }     from '@angular/common';
 import { NgModule }         from '@angular/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
-import { NgxGraphModule }   from '@swimlane/ngx-graph';
 import { GraphComponent }   from './graph.component';
-
 
 @NgModule({
   declarations: [
@@ -11,7 +9,6 @@ import { GraphComponent }   from './graph.component';
   ],
   imports:      [
     CommonModule,
-    NgxGraphModule,
     FlexLayoutModule
   ],
   exports:      [
