e6139838980cde2a6aac813b1e1c2183f73974d4
Vlady Yakovenko <vlady.y@live.it>
2022-01-19 17:25:54 +0100

    chore: cleaning

--- src/app/features/app-features.module.ts
+++ src/app/features/app-features.module.ts
@@ -3,10 +3,8 @@ import { NgModule }            from '@angular/core';
 import { BackendModule }       from 'src/app/features/backend/backend.module';
 import { ModuleBrowserModule } from 'src/app/features/module-browser/module-browser.module';
 import { PatchBrowserModule }  from 'src/app/features/patch-browser/patch-browser.module';
-import { PatchBuilderModule }  from 'src/app/features/patch-builder/patch-builder.module';
 import { RackBrowserModule }   from 'src/app/features/rack-browser/rack-browser.module';
 import { UserAreaModule }      from 'src/app/features/user-area/user-area.module';
-
 
 @NgModule({
   declarations: [],
@@ -16,7 +14,6 @@ import { UserAreaModule }      from 'src/app/features/user-area/user-area.module
     BackendModule,
     PatchBrowserModule,
     RackBrowserModule,
-    PatchBuilderModule,
     ModuleBrowserModule,
     UserAreaModule
   ]
