097526daf1a0a4b7d1e04d89e64adc2d34ef3af9
Vlady Yakovenko <vlady.y@live.it>
2022-01-19 17:21:58 +0100

    chore: cleaning

--- src/app/features/app-features.module.ts
+++ src/app/features/app-features.module.ts
@@ -3,7 +3,7 @@ import { NgModule }            from '@angular/core';
 import { BackendModule }       from 'src/app/features/backend/backend.module';
 import { ModuleBrowserModule } from 'src/app/features/module-browser/module-browser.module';
 import { PatchBrowserModule }  from 'src/app/features/patch-browser/patch-browser.module';
-import { PatchBuilderModule }  from 'src/app/features/patchBuilder/patch-builder.module';
+import { PatchBuilderModule }  from 'src/app/features/patch-builder/patch-builder.module';
 import { RackBrowserModule }   from 'src/app/features/rack-browser/rack-browser.module';
 import { UserAreaModule }      from 'src/app/features/user-area/user-area.module';
 
