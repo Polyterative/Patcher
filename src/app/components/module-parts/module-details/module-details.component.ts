2566bf9a669974149e8735dac3103bbf7935aa05
Vlady Yakovenko <vlady.y@live.it>
2022-01-20 17:59:21 +0100

    feat: module page callouts for contributions

--- src/app/components/module-parts/module-details/module-details.component.ts
+++ src/app/components/module-parts/module-details/module-details.component.ts
@@ -3,18 +3,26 @@ import {
   Component,
   Input,
   OnInit
-}                          from '@angular/core';
-import { SupabaseService } from 'src/app/features/backend/supabase.service';
-import { DbModule }        from '../../../models/module';
+}                                 from '@angular/core';
+import { fadeInOnEnterAnimation } from 'angular-animations';
+import { SupabaseService }        from 'src/app/features/backend/supabase.service';
+import { DbModule }               from '../../../models/module';
 import {
   defaultModuleMinimalViewConfig,
   ModuleMinimalViewConfig
-}                          from '../module-minimal/module-minimal.component';
+}                                 from '../module-minimal/module-minimal.component';
 
 @Component({
   selector:        'app-module-details',
   templateUrl:     './module-details.component.html',
   styleUrls:       ['./module-details.component.scss'],
+  animations:      [
+    fadeInOnEnterAnimation({
+      duration: 8000,
+      delay:    0,
+      anchor:   'help'
+    })
+  ],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ModuleDetailsComponent implements OnInit {
