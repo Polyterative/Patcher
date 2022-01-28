2870a5ab8e5f07175b6fa1e8c8b47511901416e0
Vlady Yakovenko <vlady.y@live.it>
2022-01-28 14:39:01 +0100

    chore: cleaning

--- src/app/shared-interproject/components/@visual/graph-view/graph-view.service.ts
+++ src/app/shared-interproject/components/@visual/graph-view/graph-view.service.ts
@@ -6,11 +6,5 @@ export class GraphViewService {
   readonly selectedNode$: BehaviorSubject<any | undefined> = new BehaviorSubject<any>(undefined);
   
   constructor() {
-    this.selectedNode$.subscribe(node => {
-      console.log('selected node', node);
-      
-      if (node) {
-      }
-    });
   }
 }
