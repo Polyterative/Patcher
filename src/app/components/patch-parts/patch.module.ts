4ce367ddcd01eb356c335f5cb204d4c015be1955
Vlady Yakovenko <vlady.y@live.it>
2022-01-28 12:11:49 +0100

    chore: refactoring moving code

--- src/app/components/patch-parts/patch.module.ts
+++ src/app/components/patch-parts/patch.module.ts
@@ -23,19 +23,19 @@ import { PatchMinimalComponent }                          from 'src/app/componen
 import { SharedAtomsModule }                              from 'src/app/components/shared-atoms/shared-atoms.module';
 import { UserModulesModule }                              from 'src/app/features/user-area/user-modules/user-modules.module';
 import { DevOnlyWindowModule }                            from 'src/app/shared-interproject/components/@smart/dev-only-window/dev-only-window.module';
-import { MatFormEntityModule }                            from 'src/app/shared-interproject/components/@smart/mat-form-entity/mat-form-entity.module';
-import { AdviceTooltipModule }                            from 'src/app/shared-interproject/components/@visual/advice-tooltip/advice-tooltip.module';
-import { BrandPrimaryButtonModule }                       from 'src/app/shared-interproject/components/@visual/brand-primary-button/brand-primary-button.module';
-import { HeroClickableTitleModule }                       from 'src/app/shared-interproject/components/@visual/hero-clickable-title/hero-clickable-title.module';
-import { HeroContentCardModule }                          from 'src/app/shared-interproject/components/@visual/hero-content-card/hero-content-card.module';
-import { HeroInfoBoxModule }                              from 'src/app/shared-interproject/components/@visual/hero-info-box/hero-info-box.module';
-import { ConfirmDialogModule }                            from 'src/app/shared-interproject/dialogs/confirm-dialog/confirm-dialog.module';
-import { AutoContentLoadingIndicatorModule }              from '../../shared-interproject/components/@smart/auto-content-loading-indicator/auto-content-loading-indicator.module';
-import { AutoUpdateLoadingIndicatorModule }               from '../../shared-interproject/components/@smart/auto-update-loading-indicator/auto-update-loading-indicator.module';
-import { CleanCardModule }                                from '../../shared-interproject/components/@visual/clean-card/clean-card.module';
-import { LibGraphModule }                                 from '../../shared-interproject/components/@visual/graph-view/lib-graph.module';
-import { PatchConnectionsListComponent }                  from './patch-editor/patch-connections-list/patch-connections-list.component';
-import { PatchGraphComponent }                            from './patch-graph/patch-graph.component';
+import { MatFormEntityModule }               from 'src/app/shared-interproject/components/@smart/mat-form-entity/mat-form-entity.module';
+import { AdviceTooltipModule }               from 'src/app/shared-interproject/components/@visual/advice-tooltip/advice-tooltip.module';
+import { BrandPrimaryButtonModule }          from 'src/app/shared-interproject/components/@visual/brand-primary-button/brand-primary-button.module';
+import { HeroClickableTitleModule }          from 'src/app/shared-interproject/components/@visual/hero-clickable-title/hero-clickable-title.module';
+import { HeroContentCardModule }             from 'src/app/shared-interproject/components/@visual/hero-content-card/hero-content-card.module';
+import { HeroInfoBoxModule }                 from 'src/app/shared-interproject/components/@visual/hero-info-box/hero-info-box.module';
+import { ConfirmDialogModule }               from 'src/app/shared-interproject/dialogs/confirm-dialog/confirm-dialog.module';
+import { AutoContentLoadingIndicatorModule } from '../../shared-interproject/components/@smart/auto-content-loading-indicator/auto-content-loading-indicator.module';
+import { AutoUpdateLoadingIndicatorModule }  from '../../shared-interproject/components/@smart/auto-update-loading-indicator/auto-update-loading-indicator.module';
+import { CleanCardModule }                   from '../../shared-interproject/components/@visual/clean-card/clean-card.module';
+import { LibGraphModule }                    from '../../shared-interproject/components/@visual/graph-view/lib-graph.module';
+import { PatchConnectionsListComponent }     from './patch-connections-list/patch-connections-list.component';
+import { PatchGraphComponent }               from './patch-graph/patch-graph.component';
 
 @NgModule({
   declarations: [
