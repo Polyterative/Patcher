cd7b495479d20929e42f4d8b64fc4167d9755309
Vlady Yakovenko <vlady.y@live.it>
2022-01-30 12:47:41 +0100

    fix: dialogs backdrop

--- src/app/features/app-features.module.ts
+++ src/app/features/app-features.module.ts
@@ -1,14 +1,24 @@
-import { CommonModule }        from '@angular/common';
-import { NgModule }            from '@angular/core';
-import { BackendModule }       from 'src/app/features/backend/backend.module';
-import { ModuleBrowserModule } from 'src/app/features/module-browser/module-browser.module';
-import { PatchBrowserModule }  from 'src/app/features/patch-browser/patch-browser.module';
-import { RackBrowserModule }   from 'src/app/features/rack-browser/rack-browser.module';
-import { UserAreaModule }      from 'src/app/features/user-area/user-area.module';
+import { CommonModule }               from '@angular/common';
+import { NgModule }                   from '@angular/core';
+import { MAT_DIALOG_DEFAULT_OPTIONS } from '@angular/material/dialog';
+import { BackendModule }              from 'src/app/features/backend/backend.module';
+import { ModuleBrowserModule }        from 'src/app/features/module-browser/module-browser.module';
+import { PatchBrowserModule }         from 'src/app/features/patch-browser/patch-browser.module';
+import { RackBrowserModule }          from 'src/app/features/rack-browser/rack-browser.module';
+import { UserAreaModule }             from 'src/app/features/user-area/user-area.module';
 
 @NgModule({
   declarations: [],
-  providers:    [],
+  providers:    [
+    {
+      provide:  MAT_DIALOG_DEFAULT_OPTIONS,
+      useValue: {
+        hasBackdrop:       true,
+        disableClose:      false,
+        closeOnNavigation: true
+      }
+    }
+  ],
   imports:      [
     CommonModule,
     BackendModule,
