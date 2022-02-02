23887060ff2a784d48c6a43ef67b0b5e0652fca5
Vlady Yakovenko <vlady.y@live.it>
2022-02-02 19:00:34 +0100

    feat:  patch CV list on the left

--- src/app/features/patch-browser/patch-composite/patch-composite.component.ts
+++ src/app/features/patch-browser/patch-composite/patch-composite.component.ts
@@ -3,13 +3,14 @@ import {
   Component,
   Input,
   OnInit
-}                          from '@angular/core';
+}                                 from '@angular/core';
+import { PatchDetailDataService } from '../../../components/patch-parts/patch-detail-data.service';
 import {
   defaultPatchMinimalViewConfig,
   PatchMinimalViewConfig
-}                          from '../../../components/patch-parts/patch-minimal/patch-minimal.component';
-import { Patch }           from '../../../models/patch';
-import { AppStateService } from '../../../shared-interproject/app-state.service';
+}                                 from '../../../components/patch-parts/patch-minimal/patch-minimal.component';
+import { Patch }                  from '../../../models/patch';
+import { AppStateService }        from '../../../shared-interproject/app-state.service';
 
 @Component({
   selector:        'app-patch-composite',
@@ -22,7 +23,9 @@ export class PatchCompositeComponent implements OnInit {
   @Input() isEditing: boolean = false;
   @Input() viewConfig: PatchMinimalViewConfig = defaultPatchMinimalViewConfig;
   
-  constructor(public appState: AppStateService
+  constructor(
+    public appState: AppStateService,
+    public dataService: PatchDetailDataService
   ) {}
   
   ngOnInit(): void {
