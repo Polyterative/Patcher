cf08f6e41d75b97a979f5fbc7ce9c1207d64005c
Vlady Yakovenko <vlady.y@live.it>
2022-02-06 15:55:54 +0100

    fix(changelog page): initial work

---
+++ src/app/features/info-pages/info-pages.module.ts
@@ -0,0 +1,26 @@
+import { CommonModule }       from '@angular/common';
+import { NgModule }           from '@angular/core';
+import { RouterModule }       from '@angular/router';
+import { MarkdownModule }     from 'ngx-markdown';
+import { ChangelogComponent } from './changelog/changelog.component';
+
+@NgModule({
+  declarations: [
+    ChangelogComponent
+  ],
+  imports:      [
+    CommonModule,
+    
+    RouterModule.forChild([
+      {
+        path:      'info/changelog',
+        component: ChangelogComponent
+      }
+    ]),
+    MarkdownModule
+  ],
+  exports:      [
+    ChangelogComponent
+  ]
+})
+export class InfoPagesModule {}
