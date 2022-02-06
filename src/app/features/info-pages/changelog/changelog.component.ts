5b68653bbf142b32172693d916894c05b883cad7
Vlady Yakovenko <vlady.y@live.it>
2022-02-06 15:55:54 +0100

    fix(changelog page): initial work

---
+++ src/app/features/info-pages/changelog/changelog.component.ts
@@ -0,0 +1,20 @@
+import {
+  ChangeDetectionStrategy,
+  Component,
+  OnInit
+} from '@angular/core';
+
+@Component({
+  selector:        'app-changelog',
+  templateUrl:     './changelog.component.html',
+  styleUrls:       ['./changelog.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class ChangelogComponent implements OnInit {
+  
+  constructor() { }
+  
+  ngOnInit(): void {
+  }
+  
+}
