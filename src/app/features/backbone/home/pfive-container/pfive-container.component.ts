d60627cd173296f84a9bcf34213eaf7c6993b0d3
Vlady Yakovenko <vlady.y@live.it>
2022-04-07 20:38:09 +0200

    progress

--- src/app/features/backbone/home/pfive-container/pfive-container.component.ts
+++ src/app/features/backbone/home/pfive-container/pfive-container.component.ts
@@ -5,6 +5,24 @@ import {
   OnInit
 }         from '@angular/core';
 import p5 from 'p5';
+import {
+  BehaviorSubject,
+  bufferCount,
+  interval,
+  Subject
+}         from 'rxjs';
+import {
+  share,
+  takeUntil,
+  withLatestFrom
+}         from 'rxjs/operators';
+
+interface MyGenerator {
+  
+  life: number;
+  
+  draw(p: p5, time: number): void;
+}
 
 @Component({
   selector:        'app-pfive-container',
@@ -13,27 +31,136 @@ import p5 from 'p5';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class PfiveContainerComponent implements OnInit, OnDestroy {
+  fps = 144;
   
-  private p5;
+  // rxjs clock
+  interval$ = interval(1000 / this.fps)
+    .pipe(share());
+  
+  private p5: p5;
   private origin = {
     x: 0,
     y: 0
   };
   private toggle = true;
   
+  private unit = 8;
+  
+  private generators$ = new BehaviorSubject<MyGenerator[]>([]);
+  
+  private seed = 0;
+  
+  private destroy$ = new Subject<void>();
+  
+  // private generators$ = {
+  //   sin:   (x) => {
+  //     return Math.sin(x);
+  //   },
+  //   cos:   (x) => {
+  //     return Math.cos(x);
+  //   },
+  //   tri:   (x) => {
+  //     return Math.abs(Math.sin(x));
+  //   },
+  //   sqr:   (x) => {
+  //     return Math.abs(Math.sin(x)) > 0.5 ? 1 : -1;
+  //   },
+  //   saw:   (x) => {
+  //     return Math.abs(Math.sin(x));
+  //   },
+  //   noise: (x) => {
+  //     return Math.random() * 2 - 1;
+  //   }
+  // };
+  
   constructor() {
     window.onresize = this.onWindowResize;
   }
   
   ngOnInit() {
-    console.log('digital-init');
     this.createCanvas();
+    
+    this.interval$
+        .pipe(
+          withLatestFrom(this.generators$),
+          // delay(250)
+          takeUntil(this.destroy$)
+        )
+        .subscribe(([x, generators]) => {
+          this.seed = x;
+      
+          generators.forEach(generator => {
+            generator.life = generator.life - 1;
+          });
+      
+          // remove dead generators
+          generators = generators.filter(generator => generator.life > 0);
+      
+          // clear canvas
+          this.p5.background(0);
+      
+          generators.forEach(generator => {
+            generator.draw(this.p5, this.seed);
+          });
+      
+          this.generators$.next(generators);
+      
+        });
+    
+    this.generators$.next([
+      this.circleAlgo()
+    ]);
+    
+    // add generator every 5 seconds
+    this.interval$
+        .pipe(
+          bufferCount(this.secondsToFrames(1)),
+          takeUntil(this.destroy$)
+        )
+        .subscribe(() => {
+          this.generators$.next([
+            ...this.generators$.value,
+            this.circleAlgo()
+          ]);
+        });
   }
   
   ngOnDestroy(): void {
+    
+    this.destroy$.next();
+    this.destroy$.complete();
+    // destroy p5
+    this.p5.remove();
+    
+    // unsubscribe
   }
   
-  private onWindowResize = (e) => {
+  private circleAlgo(): { draw(p): void; life: number } {
+    return {
+      draw: p => {
+        // p.background(0);
+        
+        p.stroke(100);
+        // draw a circle in the center
+        p.ellipse(this.origin.x, this.origin.y, this.unit, this.unit);
+        
+        p.ellipse(
+          p.windowWidth / 2,
+          p.windowWidth / 2,
+          this.unit * this.seed / 10,
+          this.unit * this.seed / 10
+        );
+        
+      },
+      life: this.secondsToFrames(0.5)
+    };
+  }
+  
+  private secondsToFrames(seconds: number): number {
+    return seconds * this.fps;
+  }
+  
+  private onWindowResize = e => {
     this.p5.resizeCanvas(this.p5.windowWidth, this.p5.windowHeight);
   };
   
@@ -41,6 +168,7 @@ export class PfiveContainerComponent implements OnInit, OnDestroy {
     console.log('creating canvas');
     if (this.toggle) {
       this.p5 = new p5(this.drawing);
+      this.p5.noLoop();
       this.toggle = !this.toggle;
     } else {
       this.p5.noCanvas();
@@ -48,33 +176,34 @@ export class PfiveContainerComponent implements OnInit, OnDestroy {
     }
   };
   
-  private drawing = function (p: p5) {
+  private drawing(p: p5) {
+    
+    console.log('drawing');
+    
     p.setup = () => {
       p.createCanvas(p.windowWidth, p.windowHeight)
-       .parent('digital-watch-canvas');
-      p.background(0);
+       .parent('canvas');
+      // p.background(0);
     };
     
-    p.draw = () => {
-      const time = {
-        hr: p.hour(),
-        mn: p.minute(),
-        sc: p.second(),
-        ms: p.millis()
-      };
-      const center = {
-        x: p.width / 2,
-        y: p.height / 2
-      };
-      
-      p.background(0);
-      
-      const clock = time.hr + ':' + time.mn + ':' + time.sc;
-      p.fill(255);
-      p.noStroke();
-      p.textSize(50);
-      p.text(clock, center.x, center.y);
-    };
-  };
+    // this.origin.x = p.windowWidth / 2;
+    // this.origin.y = p.windowHeight / 2;
+    
+    // p.draw = () => {
+    //   p.background(220);
+    //
+    //   const height: number = p.windowHeight;
+    //   const width: number = p.windowWidth;
+    //
+    //   const unit = 32;
+    //
+    //   console.log(height, width);
+    //
+    //   this.generators$.value.forEach(generator => {
+    //     generator.draw(p, this.seed);
+    //   });
+    //
+    // };
+  }
   
 }
