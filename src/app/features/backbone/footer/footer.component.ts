f67ed1af600898479978e2e790287088cab3bace
Vlady Yakovenko <vlady.y@live.it>
2024-01-26 00:57:16 +0100

    feat(footer): cleanup + add links

--- src/app/features/backbone/footer/footer.component.ts
+++ src/app/features/backbone/footer/footer.component.ts
@@ -1,16 +1,19 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
+import build from "../../../../build";
 
 @Component({
-    selector:        'app-footer',
-    templateUrl:     './footer.component.html',
-    styleUrls:       ['./footer.component.scss'],
-    changeDetection: ChangeDetectionStrategy.OnPush
+  selector: 'app-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class FooterComponent implements OnInit {
-    
-    constructor() { }
-    
-    ngOnInit(): void {
-    }
-    
+  data = build;
+  
+  constructor() {
+  }
+  
+  ngOnInit(): void {
+  }
+  
 }
