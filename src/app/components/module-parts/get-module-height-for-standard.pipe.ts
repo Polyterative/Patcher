aa818112d39d4e9b8060e18605ea2259b4c7fbfa
Polyterative <vlady.y@live.it>
2024-05-18 18:27:27 +0200

    feat(rack-details): more accurate 1U height

--- src/app/components/module-parts/get-module-height-for-standard.pipe.ts
+++ src/app/components/module-parts/get-module-height-for-standard.pipe.ts
@@ -1,5 +1,9 @@
-import { Pipe, PipeTransform } from '@angular/core';
+import {
+  Pipe,
+  PipeTransform
+} from '@angular/core';
 import { Standard } from '../../models/standard';
+
 
 /**
  * Output in REM
@@ -10,10 +14,8 @@ import { Standard } from '../../models/standard';
 export class GetModuleHeightForStandardPipe implements PipeTransform {
   
   transform(standard: Standard): number {
-    // let calculated3UHeight: number = 26.25;
     let visuallyFound3UHeight: number = 25.4;
-    return (standard.id == 0) || (standard.id == 1000) ? visuallyFound3UHeight : 8.6;
-    // return (standard.id == 0) || (standard.id == 1000) ? calculated1UHeight : 8.7;
+    let visuallyFound1UHeight: number = 7.6;
+    return (standard.id == 0) || (standard.id == 1000) ? visuallyFound3UHeight : visuallyFound1UHeight;
   }
-  
 }
