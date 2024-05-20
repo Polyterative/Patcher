1fb4935eb54c1db097febdc52d2c03f4fd5a069a
Polyterative <vlady.y@live.it>
2024-05-20 13:49:05 +0200

    feat(module-submit): Submit similar module button

--- src/app/features/module-browser/module-browser-adder/module-browser-adder.component.ts
+++ src/app/features/module-browser/module-browser-adder/module-browser-adder.component.ts
@@ -3,18 +3,25 @@ import {
   Component,
   Input,
   OnInit
-} from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { SeoSocialShareData } from 'ngx-seo';
-import { Subject } from 'rxjs';
+}                                 from '@angular/core';
+import { ActivatedRoute }         from '@angular/router';
+import { SeoSocialShareData }     from 'ngx-seo';
+import {
+  combineLatest,
+  Subject
+}                                 from 'rxjs';
 import {
   defaultModuleMinimalViewConfig,
   ModuleMinimalViewConfig
-} from 'src/app/components/module-parts/module-minimal/module-minimal.component';
-import { FileDragHostService } from 'src/app/shared-interproject/components/@smart/file-drag-host/file-drag-host.service';
-import { SeoAndUtilsService } from '../../backbone/seo-and-utils.service';
+}                                 from 'src/app/components/module-parts/module-minimal/module-minimal.component';
+import { FileDragHostService }    from 'src/app/shared-interproject/components/@smart/file-drag-host/file-drag-host.service';
+import { SeoAndUtilsService }     from '../../backbone/seo-and-utils.service';
 import { ModuleAdderDataService } from './module-adder-data.service';
-import { UserManagementService } from "src/app/features/backbone/login/user-management.service";
+import { UserManagementService }  from "src/app/features/backbone/login/user-management.service";
+import {
+  filter,
+  takeUntil
+}                                 from "rxjs/operators";
 
 
 @Component({
@@ -58,6 +65,46 @@ export class ModuleBrowserAdderComponent implements OnInit {
     //       // debugger
     //       this.dataService.updateSingleModuleData$.next(data);
     //     });
+    
+    // use parameters to get prefill data
+    // reference:
+    // [href]="'/modules/add?manufacturer='+bag.data.manufacturer.id+'&HP='+bag.data.hp+'standard='+bag.data.standard.id"
+    
+    combineLatest([
+      this.route.queryParams,
+      this.dataService.formData.manufacturer.options$.pipe(
+        filter(x => x.length > 0),
+        takeUntil(this.destroyEvent$)
+      ),
+      this.dataService.formData.standard.options$.pipe(
+        filter(x => x.length > 0),
+        takeUntil(this.destroyEvent$)
+      ),
+    ])
+      .pipe(
+        takeUntil(this.destroyEvent$),
+      )
+      .subscribe(([params, manufacturersList, standardsList]) => {
+          console.log([params, manufacturersList, standardsList]);
+          if (parseInt(params.manufacturer)) {
+            this.dataService.formData.manufacturer.control.setValue(
+              manufacturersList.find(x => x.id === params.manufacturer)
+            );
+          }
+          
+          if (parseInt(params.HP)) {
+            this.dataService.formData.hp.control.setValue(parseInt(params.HP));
+          }
+          
+          if (parseInt(params.standard)) {
+            this.dataService.formData.standard.control.setValue(
+              standardsList.find(x => x.id === parseInt(params.standard))
+            );
+          }
+        }
+      );
+    
+      
     
     this.updateSeo();
     
