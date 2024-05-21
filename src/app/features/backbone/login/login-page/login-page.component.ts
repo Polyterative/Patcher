59d806987fc4569db9da3ebc4395e52e41c4795d
Polyterative <vlady.y@live.it>
2024-05-21 17:59:06 +0200

    feat(login): improvements

--- src/app/features/backbone/login/login-page/login-page.component.ts
+++ src/app/features/backbone/login/login-page/login-page.component.ts
@@ -2,9 +2,18 @@ import {
   ChangeDetectionStrategy,
   Component,
   OnInit
-} from '@angular/core';
-import { UserLoginDataService } from './user-login-data.service';
-import { SeoAndUtilsService } from "src/app/features/backbone/seo-and-utils.service";
+}                                from '@angular/core';
+import { UserLoginDataService }  from './user-login-data.service';
+import { SeoAndUtilsService }    from "src/app/features/backbone/seo-and-utils.service";
+import { UserManagementService } from "src/app/features/backbone/login/user-management.service";
+import { Router }                from "@angular/router";
+import { SubManager }            from "src/app/shared-interproject/directives/subscription-manager";
+import {
+  take,
+  takeUntil
+}                                from "rxjs/operators";
+import { SharedConstants }       from "src/app/shared-interproject/SharedConstants";
+import { MatSnackBar }           from "@angular/material/snack-bar";
 
 
 @Component({
@@ -13,13 +22,17 @@ import { SeoAndUtilsService } from "src/app/features/backbone/seo-and-utils.serv
   styleUrls:       ['./login-page.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class LoginPageComponent implements OnInit {
+export class LoginPageComponent extends SubManager implements OnInit {
   
   constructor(
     public dataService: UserLoginDataService,
-    private seoAndUtilsService: SeoAndUtilsService
+    private seoAndUtilsService: SeoAndUtilsService,
+    public loginInteraction: UserManagementService,
+    private router: Router,
+    private snackBar: MatSnackBar
     
   ) {
+    super();
     this.seoAndUtilsService.updateSeo({}, 'Login');
   
     // this.activated.url.subscribe(x => {
@@ -30,6 +43,18 @@ export class LoginPageComponent implements OnInit {
   }
   
   ngOnInit(): void {
+    // if user is logged in, redirect to user area
+    this.loginInteraction.loggedUser$
+      .pipe(
+        take(1),
+        takeUntil(this.destroy$)
+      )
+      .subscribe(x => {
+        if (x) {
+          SharedConstants.successLogin(this.snackBar);
+          this.router.navigate(['/user/area']);
+        }
+      });
   }
   
 }
