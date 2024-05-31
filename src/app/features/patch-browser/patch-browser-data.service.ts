b5db1c86988b83691e8dfa73c2ddd4a644a17da9
Polyterative <vlady.y@live.it>
2024-05-31 15:07:40 +0200

    fix(browsers): starting number of elements now correct on first opening

--- src/app/features/patch-browser/patch-browser-data.service.ts
+++ src/app/features/patch-browser/patch-browser-data.service.ts
@@ -39,7 +39,7 @@ export class PatchBrowserDataService implements OnDestroy {
   ////
   serversideTableRequestData = {
     skip$:   new BehaviorSubject<number>(0),
-    take$:   new BehaviorSubject<number>(10),
+    take$: new BehaviorSubject<number>(20),
     filter$: new BehaviorSubject<string>(''),
     sort$:   new BehaviorSubject<[string, string]>([
       '',
