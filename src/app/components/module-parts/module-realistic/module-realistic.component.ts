80d79a6178614b726d28874e094cc2578de60382
Polyterative <vlady.y@live.it>
2024-06-01 14:20:57 +0200

    feat(rack-details): racks analytic view UI improvements

--- src/app/components/module-parts/module-realistic/module-realistic.component.ts
+++ src/app/components/module-parts/module-realistic/module-realistic.component.ts
@@ -1,7 +1,13 @@
-import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  Input,
+  OnInit
+} from '@angular/core';
 import { MinimalModule } from 'src/app/models/module';
 import { RackDetailDataService } from '../../rack-parts/rack-detail-data.service';
 import { ModuleDetailDataService } from '../module-detail-data.service';
+
 
 @Component({
   selector:        'app-module-realistic',
@@ -23,4 +29,11 @@ export class ModuleRealisticComponent implements OnInit {
     
   }
   
+  calculateTextSize(data: MinimalModule) {
+    const nameLength = data.name.length;
+    const hp = data.hp;
+    const standardId = data.standard.id;
+    
+    return (1 / (nameLength / 12) * (hp / 14)) + (standardId === 0 ? 0.5 : -1);
+  }
 }
