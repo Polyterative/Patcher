868bc5c2e1281a80bbd021c597b5ea7bbbc8ca10
Polyterative <vlady.y@live.it>
2024-10-26 14:10:16 +0200

    fix(backend): environment fixes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   supabase: {
-    url: '', // SECRET, comes from the build script
-    key: '' // SECRET, comes from the build script
+    url: '', // RESTful endpoint for querying and managing your database
+    key: '' // This key is safe to use in a browser if you have enabled Row Level Security for your tables and configured policies.
   },
 };
