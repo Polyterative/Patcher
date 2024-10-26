3f7f0541cc1c15c97d9e49e9fc44cd51d6ab55f6
Polyterative <vlady.y@live.it>
2024-10-26 14:33:20 +0200

    feat(backend): sentry lib update

--- generate-env.js
+++ generate-env.js
@@ -6,6 +6,7 @@ export const environment = {
   supabase: {
     url: '${process.env.SUPABASE_URL}', // SAFE to expose
     key: '${process.env.SUPABASE_ANON_KEY}' // SAFE to expose
+    
   },
 };
 `;
