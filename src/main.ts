a5100e4765da48c2b7c63506c243f8de55965f1b
Polyterative <vlady.y@live.it>
2024-10-26 12:49:58 +0200

    feat(backend): cleaner sentry code

--- src/main.ts
+++ src/main.ts
@@ -1,55 +1,24 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import * as Sentry from '@sentry/angular';
-import { Integrations } from '@sentry/tracing';
+import * as Sentry from '@sentry/browser';
 
 import { AppModule } from './app/app.module';
 import build from './build';
 import { environment } from './environments/environment';
 
-let sentry;
 
 if (environment.production) {
   enableProdMode();
   
-  sentry = {
-    environment:      'production',
-    release:          'patcher@' + build.version,
-    dsn:              'https://57dc8f0b1ad240f3afa61628b8351aae@o718439.ingest.sentry.io/5780783',
-    integrations:     [
-      new Integrations.BrowserTracing({
-        tracingOrigins:         [
-          'localhost',
-          'https://patcher.xyz'
-        ],
-        routingInstrumentation: Sentry.routingInstrumentation
-      })
-    ],
-    tracesSampleRate: 1
-  };
-  
-  Sentry.init(sentry);
-  
-} else {
-  // sentry = {
-  //   environment:      'development',
-  //   release:          'patcher@' + build.version,
-  //   dsn:              'https://57dc8f0b1ad240f3afa61628b8351aae@o718439.ingest.sentry.io/5780783',
-  //   integrations:     [
-  //     new Integrations.BrowserTracing({
-  //       tracingOrigins:         [
-  //         'localhost',
-  //         'https://patcher.xyz'
-  //       ],
-  //       routingInstrumentation: Sentry.routingInstrumentation
-  //     })
-  //   ],
-  //   tracesSampleRate: 1
-  // };
-  
+  Sentry.init({
+    dsn: 'https://57dc8f0b1ad240f3afa61628b8351aae@o718439.ingest.us.sentry.io/5780783',
+    environment: environment.production ? 'production' : 'development',
+    release: `patcher@${ build.version }`,
+    integrations: [Sentry.browserTracingIntegration()],
+    tracesSampleRate: 1.0,
+    tracePropagationTargets: ['localhost', /^https:\/\/patcher\.xyz/], // Updated to include only patcher.xyz
+  });
 }
-
-
 
 platformBrowserDynamic()
   .bootstrapModule(AppModule)
