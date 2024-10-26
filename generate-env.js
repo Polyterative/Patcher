8f00f44f093c9dbe086afa725496e417e7bc6c4b
Polyterative <vlady.y@live.it>
2024-10-26 14:10:16 +0200

    fix(backend): environment fixes

--- generate-env.js
+++ generate-env.js
@@ -4,8 +4,8 @@ const envContent = `
 export const environment = {
   production: true,
   supabase: {
-    url: '${process.env.SUPABASE_URL}', // SECRET, comes from the build script
-    key: '${process.env.SUPABASE_KEY}' // SECRET, comes from the build script
+    url: '${process.env.SUPABASE_URL}', // SAFE to expose
+    key: '${process.env.SUPABASE_ANON_KEY}' // SAFE to expose
   },
 };
 `;
