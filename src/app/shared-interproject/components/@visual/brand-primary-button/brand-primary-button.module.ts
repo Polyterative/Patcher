6caba25b855ea041517fb4a31f97caf4f4392ce2
polyterative <vlacker@live.it>
2026-02-19 13:34:37 +0100

    feat(buttons): replace mat-button with app-brand-primary-button for consistent styling and add icon support

--- src/app/shared-interproject/components/@visual/brand-primary-button/brand-primary-button.module.ts
+++ src/app/shared-interproject/components/@visual/brand-primary-button/brand-primary-button.module.ts
@@ -5,6 +5,7 @@ import { FlexLayoutModule } from '@angular/flex-layout';
 import { RouterModule } from '@angular/router';
 import { BrandPrimaryButtonComponent } from './brand-primary-button.component';
 import { MatButtonModule } from "@angular/material/button";
+import { MatIconModule } from "@angular/material/icon";
 
 
 @NgModule({
@@ -13,6 +14,7 @@ import { MatButtonModule } from "@angular/material/button";
     CommonModule,
     FlexLayoutModule,
     MatButtonModule,
+    MatIconModule,
     RouterModule,
     A11yModule,
   ],
