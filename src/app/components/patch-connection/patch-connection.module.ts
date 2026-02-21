18ab4e837c196218a16176e49766bbed4ca613e6
polyterative <vlacker@live.it>
2026-02-21 18:29:09 +0100

    fix(patch-editor): prevent auto-save on patch load

--- src/app/components/patch-connection/patch-connection.module.ts
+++ src/app/components/patch-connection/patch-connection.module.ts
@@ -7,7 +7,6 @@ import { MatFormEntityComponent } from 'src/app/shared-interproject/components/@
 import { BrandPrimaryButtonModule } from 'src/app/shared-interproject/components/@visual/brand-primary-button/brand-primary-button.module';
 import { HeroInfoBoxModule } from 'src/app/shared-interproject/components/@visual/hero-info-box/hero-info-box.module';
 import { PatchConnectionMinimalComponent } from './patch-connection-minimal/patch-connection-minimal.component';
-import { MatChipsModule } from "@angular/material/chips";
 import { MatIconModule } from "@angular/material/icon";
 import { MatTooltipModule } from "@angular/material/tooltip";
 import { MatButtonModule } from "@angular/material/button";
@@ -23,7 +22,6 @@ import { MatCardModule } from "@angular/material/card";
   imports: [
     CommonModule,
     
-    MatChipsModule,
     ModulePartsModule,
     FlexLayoutModule,
     SharedAtomsModule,
