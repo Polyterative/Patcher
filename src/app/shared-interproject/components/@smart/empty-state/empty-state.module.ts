5cd45573f27edfbcc9f06429726d76f1d214c8f9
polyterative <vlacker@live.it>
2026-02-21 23:54:24 +0100

    feat(empty-state): add empty state component with icon and message

--- src/app/shared-interproject/components/@smart/empty-state/empty-state.module.ts
+++ src/app/shared-interproject/components/@smart/empty-state/empty-state.module.ts
@@ -1,14 +1,17 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatIconModule } from '@angular/material/icon';
 import { LottieContainerModule } from '../lottie-container/lottie-container.module';
 import { EmptyStateComponent } from './empty-state.component';
+
 
 @NgModule({
   declarations: [EmptyStateComponent],
   imports:      [
     CommonModule,
     FlexLayoutModule,
+    MatIconModule,
     LottieContainerModule
   ],
   exports:      [EmptyStateComponent]
