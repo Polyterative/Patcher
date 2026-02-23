8bbc63a5822ca8b316acddd52a44d8e9db53e585
polyterative <vlacker@live.it>
2026-02-23 09:58:29 +0100

    Fix top five dependency alerts

--- src/app/features/backend/admin-panel-root/admin-panel-root.component.ts
+++ src/app/features/backend/admin-panel-root/admin-panel-root.component.ts
@@ -1,8 +1,6 @@
-import { HttpClient } from '@angular/common/http';
 import {
   ChangeDetectionStrategy,
-  Component,
-  OnInit
+  Component
 } from '@angular/core';
 import { Subject } from 'rxjs';
 import { normalizeForSearch } from 'src/app/shared-interproject/components/@smart/mat-form-entity/string-utils';
@@ -16,71 +14,54 @@ import { SupabaseService } from '../supabase.service';
   changeDetection: ChangeDetectionStrategy.OnPush,
   standalone: false
 })
-export class AdminPanelRootComponent implements OnInit {
+export class AdminPanelRootComponent {
   devToProd$ = new Subject<void>();
   downloadProd$ = new Subject<void>();
   downloadDev$ = new Subject<void>();
   prodToDev$ = new Subject<void>();
   click$ = new Subject<void>();
   
-  constructor(public http: HttpClient, public backend: SupabaseService) { }
-  
-  ngOnInit(): void {
-  
-    // this.click$
-    //     .pipe(
-    //       switchMap(x => zip(
-    //           this.http.get('https://api.martinpas.com/products?published=true&_limit=99999')
-    //         )
-    //       )
-    //     )
-    //     .subscribe(x => {
-    //       console.clear();
-    //       console.log(x);
-    //
-    //
-    //     });
-  
-  
-  }
-  
-  similarity(s1, s2) {
+  constructor(public backend: SupabaseService) {}
+
+  similarity(s1: string, s2: string): number {
     let longer = s1;
-    var shorter = s2;
+    let shorter = s2;
     if (s1.length < s2.length) {
       longer = s2;
       shorter = s1;
     }
-    var longerLength = longer.length;
-    if (longerLength == 0) {
+    const longerLength = longer.length;
+    if (longerLength === 0) {
       return 1.0;
     }
     return (longerLength - this.editDistance(longer, shorter)) / parseFloat(longerLength);
   }
   
-  editDistance(s1, s2) {
+  editDistance(s1: string, s2: string): number {
     s1 = normalizeForSearch(s1);
     s2 = normalizeForSearch(s2);
     
-    var costs = new Array();
-    for (var i = 0; i <= s1.length; i++) {
-      var lastValue = i;
-      for (var j = 0; j <= s2.length; j++) {
-        if (i == 0)
+    const costs: number[] = [];
+    for (let i = 0; i <= s1.length; i++) {
+      let lastValue = i;
+      for (let j = 0; j <= s2.length; j++) {
+        if (i === 0) {
           costs[j] = j;
-        else {
+        } else {
           if (j > 0) {
-            var newValue = costs[j - 1];
-            if (s1.charAt(i - 1) != s2.charAt(j - 1))
+            let newValue = costs[j - 1];
+            if (s1.charAt(i - 1) !== s2.charAt(j - 1)) {
               newValue = Math.min(Math.min(newValue, lastValue),
                 costs[j]) + 1;
+            }
             costs[j - 1] = lastValue;
             lastValue = newValue;
           }
         }
       }
-      if (i > 0)
+      if (i > 0) {
         costs[s2.length] = lastValue;
+      }
     }
     return costs[s2.length];
   }
