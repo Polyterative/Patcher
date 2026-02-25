52021c8b2f16a2f6ffac74253b32541bd4bb51fd
polyterative <vlacker@live.it>
2026-02-25 19:59:31 +0100

    feat(comments): add commentText pipe for safe HTML rendering
    Introduce a new pipe to sanitize and render
    comments as safe HTML. This enhances security
    by preventing XSS attacks and allows clickable
    links for URLs in comments.

--- src/app/components/shared-atoms/comments/comments-item/comments-item.component.ts
+++ src/app/components/shared-atoms/comments/comments-item/comments-item.component.ts
@@ -2,14 +2,11 @@ import {
   ChangeDetectionStrategy,
   Component,
   Input,
-  OnInit,
-  SecurityContext,
-  ViewEncapsulation
+  OnInit
 } from '@angular/core';
 import { DbComment } from 'src/app/models/comment';
 import { CommentsDataService } from "src/app/components/shared-atoms/comments/comments-data.service";
 import { UserManagementService } from "src/app/features/backbone/login/user-management.service";
-import { DomSanitizer } from "@angular/platform-browser";
 import {
   AsyncPipe,
   DatePipe
@@ -20,6 +17,7 @@ import { CleanCardModule } from "src/app/shared-interproject/components/@visual/
 import { MatTooltip } from "@angular/material/tooltip";
 import { MatIconButton } from "@angular/material/button";
 import { CommentContextComponent } from "src/app/components/shared-atoms/comments/comment-context/comment-context.component";
+import { CommentTextPipe } from "src/app/components/shared-atoms/comments/comment-text.pipe";
 
 
 export interface CommentViewConfig {
@@ -43,36 +41,26 @@ export const defaultCommentViewConfig: CommentViewConfig = {
     CleanCardModule,
     MatTooltip,
     MatIconButton,
-    CommentContextComponent
+    CommentContextComponent,
+    CommentTextPipe,
   ],
-  encapsulation: ViewEncapsulation.None
 })
 export class CommentsItemComponent implements OnInit {
   @Input() data: DbComment;
   currentDateTime = new Date();
   isDeletable = false;
-  
+
   @Input() viewConfig: CommentViewConfig = defaultCommentViewConfig;
   
   constructor(
     public dataService: CommentsDataService,
     public userService: UserManagementService,
-    private sanitizer: DomSanitizer
   ) {
   }
   
   ngOnInit(): void {
-    // determine if the comment is NOT deletable, only if older than 30 minutes
     const commentDate = new Date(this.data.created);
     const diff = this.currentDateTime.getTime() - commentDate.getTime();
-    const maxDiff = 30 * 60 * 1000;
-    this.isDeletable = diff < maxDiff;
-  }
-  
-  
-  makeURLsClickable(content: string) {
-    const urlRegex = /(https?:\/\/[^\s]+)/g;
-    const sanitizedContent = this.sanitizer.sanitize(SecurityContext.HTML, content);
-    return sanitizedContent.replace(urlRegex, '<a href="$1" target="_blank">$1</a>');
+    this.isDeletable = diff < 30 * 60 * 1000;
   }
 }
