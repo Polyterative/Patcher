0769af68e7074a8f3d730fb0c6ac1483855543ec
Vlady Yakovenko <vlady.y@live.it>
2026-05-13 13:33:07 +0200

    refactor: extract rack-function-visuals interfaces to models file

--- src/app/components/rack-parts/rack-function-visuals.utils.ts
+++ src/app/components/rack-parts/rack-function-visuals.utils.ts
@@ -5,37 +5,21 @@ import {
   RackBalanceAxisId
 } from './rack-balance-analysis.constants';
 import { isBlankModule } from './rack-blank-module.constants';
+import {
+  FunctionAnalysisLegendItem,
+  FunctionAnalysisLegendSummaryItem,
+  RackFunctionVisual,
+  RowFunctionBreakdown,
+  RowFunctionRoleBreakdown
+} from './rack-function-visuals.models';
 
-export interface RackFunctionVisual {
-  className: string;
-  roleLabel: string;
-  tagLabel: string;
-  icon: string;
-}
-
-export interface FunctionAnalysisLegendItem {
-  label: string;
-  swatchClass: string;
-}
-
-export interface FunctionAnalysisLegendSummaryItem extends FunctionAnalysisLegendItem {
-  count: number;
-  hp: number;
-}
-
-export interface RowFunctionRoleBreakdown {
-  label: string;
-  className: string;
-  moduleCount: number;
-  hp: number;
-}
-
-export interface RowFunctionBreakdown {
-  moduleCount: number;
-  roles: RowFunctionRoleBreakdown[];
-  residualCount: number;
-  residualHp: number;
-}
+export type {
+  FunctionAnalysisLegendItem,
+  FunctionAnalysisLegendSummaryItem,
+  RackFunctionVisual,
+  RowFunctionBreakdown,
+  RowFunctionRoleBreakdown
+} from './rack-function-visuals.models';
 
 interface RackFunctionAxisScore {
   score: number;
