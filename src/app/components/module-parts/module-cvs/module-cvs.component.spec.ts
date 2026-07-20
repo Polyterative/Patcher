581c3e7b398258dcfe6137462496cc825ef4628d
Polyterative <vlady.y@live.it>
2026-07-20 20:03:13 +0200

    test(module-cvs): remove explicit any fallbacks

--- src/app/components/module-parts/module-cvs/module-cvs.component.spec.ts
+++ src/app/components/module-parts/module-cvs/module-cvs.component.spec.ts
@@ -1,31 +1,77 @@
+import { MatSnackBar } from '@angular/material/snack-bar';
 import {
   BehaviorSubject,
   of,
   Subject,
   throwError
 } from 'rxjs';
+import { PatchModuleInstance } from 'src/app/models/connection';
+import {
+  CV,
+  CVConnectionEntity
+} from 'src/app/models/cv';
+import { DbModule } from 'src/app/models/module';
+import { PatchDetailDataService } from '../../patch-parts/patch-detail-data.service';
+import { dbModuleFixture } from '../../patch-parts/patch-graph/patch-graph-test-fixtures';
 import { ModuleCVsComponent } from './module-cvs.component';
 
 
 describe('ModuleCVsComponent', () => {
+  type EnsureModuleInstance = PatchDetailDataService['ensureModuleInstance$'];
+
+  type ModuleCvsPatchServiceDouble = PatchDetailDataService & {
+    patchEditingPanelOpenState$: BehaviorSubject<boolean>;
+    patchModuleInstances$: BehaviorSubject<PatchModuleInstance[]>;
+    clickOnModuleCV$: Subject<CVConnectionEntity>;
+    ensureModuleInstance$: jasmine.Spy<EnsureModuleInstance>;
+  };
+
+  function cvFixture(id: number, name: string): CV {
+    return {id, name};
+  }
+
+  function moduleFixture(
+    id = 10,
+    name = 'Module',
+    ins: CV[] = [],
+    outs: CV[] = []
+  ): DbModule {
+    return dbModuleFixture(id, name, ins, outs);
+  }
+
+  function patchModuleInstanceFixture(id: number, moduleId = 10): PatchModuleInstance {
+    return {
+      id,
+      patch_id: 1,
+      module_id: moduleId,
+      instance_label: null
+    };
+  }
+
+  function createPatchServiceDouble(clickOnModuleCV$: Subject<CVConnectionEntity>): ModuleCvsPatchServiceDouble {
+    return Object.assign(
+      Object.create(PatchDetailDataService.prototype) as PatchDetailDataService,
+      {
+        patchEditingPanelOpenState$: new BehaviorSubject<boolean>(true),
+        patchModuleInstances$: new BehaviorSubject<PatchModuleInstance[]>([]),
+        clickOnModuleCV$,
+        ensureModuleInstance$: jasmine.createSpy<EnsureModuleInstance>('ensureModuleInstance$')
+          .and.returnValue(of(77))
+      }
+    );
+  }
+
   function build() {
-    const clickOnModuleCV$ = new Subject<any>();
-    const snackBar = {
-      open: jasmine.createSpy('open')
-    };
-    const patchService = {
-      patchEditingPanelOpenState$: new BehaviorSubject<boolean>(true),
-      patchModuleInstances$: new BehaviorSubject<any[]>([]),
-      clickOnModuleCV$,
-      ensureModuleInstance$: jasmine.createSpy('ensureModuleInstance$').and.returnValue(of(77))
-    };
-    const component = new ModuleCVsComponent(patchService as any, snackBar as any);
-    component.data = {
-      id: 10,
-      name: 'Module',
-      ins: [{id: 1, name: 'In10'}, {id: 2, name: 'In2'}, {id: 3, name: 'In1'}],
-      outs: [{id: 4, name: 'Out2'}, {id: 5, name: 'Out10'}, {id: 6, name: 'Out1'}]
-    } as any;
+    const clickOnModuleCV$ = new Subject<CVConnectionEntity>();
+    const snackBar = jasmine.createSpyObj<MatSnackBar>('MatSnackBar', ['open']);
+    const patchService = createPatchServiceDouble(clickOnModuleCV$);
+    const component = new ModuleCVsComponent(patchService, snackBar);
+    component.data = moduleFixture(
+      10,
+      'Module',
+      [cvFixture(1, 'In10'), cvFixture(2, 'In2'), cvFixture(3, 'In1')],
+      [cvFixture(4, 'Out2'), cvFixture(5, 'Out10'), cvFixture(6, 'Out1')]
+    );
     return {component, patchService, clickOnModuleCV$, snackBar};
   }
   
@@ -40,12 +86,11 @@ describe('ModuleCVsComponent', () => {
   
   it('handles equal and pure-string CV names in sort comparator', () => {
     const {component} = build();
-    component.data = {
-      id: 10,
-      name: 'Module',
-      ins: [{id: 1, name: 'Alpha'}, {id: 2, name: 'Alpha'}, {id: 3, name: 'Beta'}],
-      outs: []
-    } as any;
+    component.data = moduleFixture(
+      10,
+      'Module',
+      [cvFixture(1, 'Alpha'), cvFixture(2, 'Alpha'), cvFixture(3, 'Beta')]
+    );
     
     component.ngOnInit();
     
@@ -54,15 +99,15 @@ describe('ModuleCVsComponent', () => {
   
   it('auto-creates and caches instance id on first click when missing', () => {
     const {component, patchService, clickOnModuleCV$} = build();
-    const emissions: any[] = [];
+    const emissions: CVConnectionEntity[] = [];
     clickOnModuleCV$.subscribe(v => emissions.push(v));
     component.instanceId = undefined;
     component.ngOnInit();
     
-    const cv = {id: 1, name: 'In1'} as any;
-    const mod = {id: 10, name: 'Module'} as any;
+    const cv = cvFixture(1, 'In1');
+    const mod = moduleFixture();
     component.inClick$.next([cv, mod]);
-    component.outClick$.next([{id: 2, name: 'Out1'} as any, mod]);
+    component.outClick$.next([cvFixture(2, 'Out1'), mod]);
     
     expect(patchService.ensureModuleInstance$).toHaveBeenCalledTimes(1);
     expect(component.instanceId).toBe(77);
@@ -78,7 +123,7 @@ describe('ModuleCVsComponent', () => {
     patchService.patchEditingPanelOpenState$.next(false);
     
     component.ngOnInit();
-    component.inClick$.next([{id: 1, name: 'In1'} as any, {id: 10, name: 'Module'} as any]);
+    component.inClick$.next([cvFixture(1, 'In1'), moduleFixture()]);
     
     expect(nextSpy).not.toHaveBeenCalled();
     expect(patchService.ensureModuleInstance$).not.toHaveBeenCalled();
@@ -91,9 +136,9 @@ describe('ModuleCVsComponent', () => {
     component.instanceId = undefined;
     component.ngOnInit();
     
-    const module = {id: 10, name: 'Module'} as any;
-    component.inClick$.next([{id: 1, name: 'In'} as any, module]);
-    component.outClick$.next([{id: 2, name: 'Out'} as any, module]);
+    const moduleData = moduleFixture();
+    component.inClick$.next([cvFixture(1, 'In'), moduleData]);
+    component.outClick$.next([cvFixture(2, 'Out'), moduleData]);
     
     expect(nextSpy).not.toHaveBeenCalled();
   });
@@ -102,13 +147,13 @@ describe('ModuleCVsComponent', () => {
     const {component, patchService, clickOnModuleCV$, snackBar} = build();
     const nextSpy = spyOn(clickOnModuleCV$, 'next').and.callThrough();
     patchService.patchModuleInstances$.next([
-      {id: 101, module_id: 10},
-      {id: 102, module_id: 10}
+      patchModuleInstanceFixture(101),
+      patchModuleInstanceFixture(102)
     ]);
     component.instanceId = undefined;
     component.ngOnInit();
 
-    component.outClick$.next([{id: 2, name: 'Out'} as any, {id: 10, name: 'Module'} as any]);
+    component.outClick$.next([cvFixture(2, 'Out'), moduleFixture()]);
 
     expect(patchService.ensureModuleInstance$).not.toHaveBeenCalled();
     expect(nextSpy).not.toHaveBeenCalled();
@@ -121,16 +166,16 @@ describe('ModuleCVsComponent', () => {
 
   it('allows clicks on a labeled card even when multiple instances exist', () => {
     const {component, patchService, clickOnModuleCV$, snackBar} = build();
-    const emissions: any[] = [];
+    const emissions: CVConnectionEntity[] = [];
     clickOnModuleCV$.subscribe(v => emissions.push(v));
     patchService.patchModuleInstances$.next([
-      {id: 101, module_id: 10},
-      {id: 102, module_id: 10}
+      patchModuleInstanceFixture(101),
+      patchModuleInstanceFixture(102)
     ]);
     component.instanceId = 102;
     component.ngOnInit();
 
-    component.inClick$.next([{id: 1, name: 'In1'} as any, {id: 10, name: 'Module'} as any]);
+    component.inClick$.next([cvFixture(1, 'In1'), moduleFixture()]);
 
     expect(patchService.ensureModuleInstance$).not.toHaveBeenCalled();
     expect(emissions.length).toBe(1);
@@ -140,8 +185,8 @@ describe('ModuleCVsComponent', () => {
   
   it('emits and completes inherited destroy subject on ngOnDestroy', () => {
     const {component} = build();
-    const nextSpy = spyOn((component as any).destroy$, 'next').and.callThrough();
-    const completeSpy = spyOn((component as any).destroy$, 'complete').and.callThrough();
+    const nextSpy = spyOn(component.destroy$, 'next').and.callThrough();
+    const completeSpy = spyOn(component.destroy$, 'complete').and.callThrough();
     
     component.ngOnDestroy();
     
