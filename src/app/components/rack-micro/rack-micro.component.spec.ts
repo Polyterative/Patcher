30ca5aa5adf94182bb36af5b71891262587b0ac9
Polyterative <vlady.y@live.it>
2026-07-21 23:11:40 +0200

    test(rack-micro): type micro rack spec fixtures

--- src/app/components/rack-micro/rack-micro.component.spec.ts
+++ src/app/components/rack-micro/rack-micro.component.spec.ts
@@ -1,10 +1,36 @@
 import { RackMicroComponent } from './rack-micro.component';
+import { RackMinimalViewConfig } from '../rack-parts/rack-minimal/rack-minimal.component';
+import { RackMinimal } from '../../models/rack';
 
 describe('RackMicroComponent', () => {
   let comp: RackMicroComponent;
+  let rack: RackMinimal;
+  let viewConfig: RackMinimalViewConfig;
 
   beforeEach(() => {
     comp = new RackMicroComponent();
+    rack = {
+      id: 5,
+      name: 'Test Rack',
+      hp: 84,
+      rows: 3,
+      author: {
+        id: 'user-1',
+        username: 'test-user'
+      },
+      locked: false,
+      public: true,
+      created: '2026-07-21T00:00:00.000Z',
+      updated: '2026-07-21T00:00:00.000Z'
+    };
+    viewConfig = {
+      hideLabels: true,
+      hideDescription: false,
+      hideButtons: false,
+      hideHP: false,
+      hideDates: false,
+      containImage: true
+    };
   });
 
   it('creates without error', () => {
@@ -16,12 +42,12 @@ describe('RackMicroComponent', () => {
   });
 
   it('data input can be assigned', () => {
-    comp.data = {id: 5, name: 'Test Rack'} as any;
+    comp.data = rack;
     expect(comp.data.id).toBe(5);
   });
 
   it('viewConfig input can be assigned', () => {
-    comp.viewConfig = {showStats: true} as any;
-    expect((comp.viewConfig as any).showStats).toBeTrue();
+    comp.viewConfig = viewConfig;
+    expect(comp.viewConfig.hideLabels).toBeTrue();
   });
 });
